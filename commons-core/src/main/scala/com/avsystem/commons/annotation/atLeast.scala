a12ee157377166c9ddf22ccfcfd46c6426564c0f
Roman Janusz <romeqjanoosh@gmail.com>
2015-09-09 11:37:25 +0200

    @atLeast doc improved

--- commons-core/src/main/scala/com/avsystem/commons/annotation/atLeast.scala
+++ commons-core/src/main/scala/com/avsystem/commons/annotation/atLeast.scala
@@ -6,5 +6,13 @@ import scala.annotation.StaticAnnotation
 /**
  * When applied on varargs parameter, indicates that at least some number of parameters is required.
  * This is later checked by the static analyzer.
+ * <br/>
+ * WARNING: implementation of method which takes a varargs parameter may NOT assume that given number of
+ * arguments will always be passed, because it's still possible to pass a [[Seq]] where varargs parameter is
+ * required using the `: _*` ascription, e.g.
+ * {{{
+ *   varargsMethod(List(): _*)
+ * }}}
+ * and that is not checked by the static analyzer.
  */
 class atLeast(n: Int) extends StaticAnnotation
