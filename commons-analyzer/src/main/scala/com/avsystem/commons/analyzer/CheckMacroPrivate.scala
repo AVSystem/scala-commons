a7e090664a62f2f5579c05cf8b0857bc7a70f986
Roman Janusz <romeqjanoosh@gmail.com>
2016-02-18 13:40:25 +0100

    more fixes to @macroPrivate checker

--- commons-analyzer/src/main/scala/com/avsystem/commons/analyzer/CheckMacroPrivate.scala
+++ commons-analyzer/src/main/scala/com/avsystem/commons/analyzer/CheckMacroPrivate.scala
@@ -16,13 +16,17 @@ class CheckMacroPrivate[C <: Global with Singleton](g: C) extends AnalyzerRule(g
   def analyze(unit: CompilationUnit) = if (macroPrivateAnnotTpe != NoType) {
     def analyzeTree(tree: Tree): Unit = analyzer.macroExpandee(tree) match {
       case `tree` | EmptyTree =>
-        val sym = tree.symbol
-        if (!tree.isDef && sym != null && tree.pos != NoPosition) {
-          val macroPrivate = (sym :: sym.overrides).iterator
-            .flatMap(_.annotations).exists(_.tree.tpe <:< macroPrivateAnnotTpe)
-          if (macroPrivate) {
-            reporter.error(tree.pos, s"$sym can only be used in macro-generated code")
-          }
+        tree match {
+          case _: Ident | _: Select | _: SelectFromTypeTree | _: Apply | _: New
+            if tree.symbol != null && tree.pos != NoPosition =>
+
+            val sym = tree.symbol
+            val macroPrivate = (sym :: sym.overrides).iterator
+              .flatMap(_.annotations).exists(_.tree.tpe <:< macroPrivateAnnotTpe)
+            if (macroPrivate) {
+              reporter.error(tree.pos, s"$sym can only be used in macro-generated code")
+            }
+          case _ =>
         }
         tree.children.foreach(analyzeTree)
       case prevTree =>
--- commons-analyzer/src/test/scala/com/avsystem/commons/analyzer/CheckMacroPrivateTest.scala
+++ commons-analyzer/src/test/scala/com/avsystem/commons/analyzer/CheckMacroPrivateTest.scala
@@ -32,13 +32,16 @@ class CheckMacroPrivateTest extends FunSuite with AnalyzerTest {
     )
   }
 
-  test("the definition of macro private method itself should not be rejected") {
+  test("definitions of macro private symbols themselves should not be rejected") {
     assertNoErrors(
       """
         |import com.avsystem.commons.annotation.macroPrivate
         |
         |object test {
-        |  @macroPrivate def macroPrivateMethod = 5
+        |  @macroPrivate def macroPrivateMethod = { println("whatever"); 5 }
+        |  @macroPrivate object macroPrivateObject {
+        |    val x = 42
+        |  }
         |}
       """.stripMargin
     )
