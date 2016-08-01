72f41794ef79bde89d87490117cc974c4c56893e
ghik <romeqjanoosh@gmail.com>
2016-08-01 13:29:10 +0200

    working around travis caching behavior

--- install-redis.sh
+++ install-redis.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ ! -d redis-$REDIS_VERSION ]; then
+if [ ! -d redis-$REDIS_VERSION/src ]; then
     wget http://download.redis.io/releases/redis-$REDIS_VERSION.tar.gz
     tar -xzf redis-$REDIS_VERSION.tar.gz
     cd redis-$REDIS_VERSION && make
