3ee455f85e1f69ad98f19b860374c9a145a59ae4
ghik <romeqjanoosh@gmail.com>
2016-08-03 17:58:11 +0200

    increased test timeouts

--- commons-redis/src/test/scala/com/avsystem/commons/redis/UsesActorSystem.scala
+++ commons-redis/src/test/scala/com/avsystem/commons/redis/UsesActorSystem.scala
@@ -3,6 +3,8 @@ package redis
 
 import akka.actor.ActorSystem
 import akka.util.Timeout
+import org.scalatest.concurrent.PatienceConfiguration
+import org.scalatest.time.{Milliseconds, Seconds, Span}
 import org.scalatest.{BeforeAndAfterAll, Suite}
 
 import scala.concurrent.duration._
@@ -12,10 +14,13 @@ import scala.concurrent.{Await, ExecutionContext}
   * Author: ghik
   * Created: 14/04/16.
   */
-trait UsesActorSystem extends BeforeAndAfterAll {this: Suite =>
+trait UsesActorSystem extends BeforeAndAfterAll with PatienceConfiguration {this: Suite =>
   implicit lazy val actorSystem: ActorSystem = ActorSystem()
   implicit def executionContext: ExecutionContext = actorSystem.dispatcher
-  implicit val timeout = Timeout(10.seconds)
+  implicit val timeout = Timeout(60.seconds)
+
+  override implicit def patienceConfig =
+    PatienceConfig(Span(60, Seconds), Span(100, Milliseconds))
 
   override protected def afterAll() = {
     Await.ready(actorSystem.terminate(), Duration.Inf)
