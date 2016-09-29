83c23b04fc9cd91d68204211833beff1270bd2cb
ghik <romeqjanoosh@gmail.com>
2016-09-29 08:07:33 +0200

    ordered enum

--- commons-macros/src/main/scala/com/avsystem/commons/macros/misc/SealedMacros.scala
+++ commons-macros/src/main/scala/com/avsystem/commons/macros/misc/SealedMacros.scala
@@ -13,12 +13,15 @@ class SealedMacros(ctx: blackbox.Context) extends AbstractMacroCommons(ctx) {
 
   import c.universe._
 
+  val OrderedEnumType = getType(tq"$CommonsPackage.misc.OrderedEnum")
+
   def caseObjectsFor[T: c.WeakTypeTag]: Tree = {
     val tpe = weakTypeOf[T]
     knownSubtypes(tpe).map { subtypes =>
       val objects = subtypes.map(subTpe => singleValueFor(subTpe)
         .getOrElse(abort(s"All possible values of a SealedEnum must be objects but $subTpe is not")))
-      withKnownSubclassesCheck(q"$ListObj(..$objects)", tpe)
+      val result = withKnownSubclassesCheck(q"$ListObj(..$objects)", tpe)
+      if (tpe <:< OrderedEnumType) q"$result.sorted" else result
     }.getOrElse(abort(s"$tpe is not a sealed trait or class"))
   }
 }
--- commons-shared/src/main/scala/com/avsystem/commons/misc/SealedUtils.scala
+++ commons-shared/src/main/scala/com/avsystem/commons/misc/SealedUtils.scala
@@ -10,8 +10,8 @@ import com.avsystem.commons.serialization.{GenCodec, GenKeyCodec}
 object SealedUtils {
   /**
     * A macro which reifies a list of all case objects of a sealed trait or class `T`.
-    * WARNING: the order of case objects in the resulting list is arbitrary and is NOT guaranteed to be consistent with
-    * declaration order.
+    * WARNING: the order of case objects in the resulting list is guaranteed to be consistent with
+    * declaration order ONLY for enums extending [[OrderedEnum]]. Otherwise, the order may be arbitrary.
     */
   def caseObjectsFor[T]: List[T] = macro macros.misc.SealedMacros.caseObjectsFor[T]
 }
@@ -42,21 +42,19 @@ trait SealedEnumCompanion[T] {
     *   val values: List[MyEnum] = caseObjects
     * }}}
     *
-    * Also, be aware that [[caseObjects]] macro does NOT guarantee any particular order of elements.
+    * Also, be aware that [[caseObjects]] macro guarantees well-defined order of elements only for [[OrderedEnum]]s.
     */
   val values: List[T]
 
   /**
     * A macro which reifies a list of all case objects of the sealed trait or class `T`.
-    * WARNING: the order of case objects in the resulting list is arbitrary and is NOT guaranteed to be consistent with
-    * declaration order.
+    * WARNING: the order of case objects in the resulting list is well defined only for enums that extend [[OrderedEnum]].
+    * In such case, the order is consistent with declaration order in source file. However, if the enum is not an
+    * [[OrderedEnum]], the order may be arbitrary.
     */
   protected def caseObjects: List[T] = macro macros.misc.SealedMacros.caseObjectsFor[T]
 }
 
-/**
-  * Base trait for companion objects of sealed traits that serve as named enums
-  */
 trait NamedEnum extends Any {
   /**
     * Used as a key for a map returned from `byName`. It is recommended to override this method uniquely
@@ -66,14 +64,45 @@ trait NamedEnum extends Any {
   override def toString: String = name
 }
 
+/**
+  * Base trait for companion objects of sealed traits that serve as named enums
+  */
 trait NamedEnumCompanion[T <: NamedEnum] extends SealedEnumCompanion[T] {
   /**
     * Returns a map from all case objects names to their instances.
     * Since `byName` uses [[caseObjects]] macro it does NOT guarantee an order of elements. It is also essential
     * to provide unique names for each case object in the sealed hierarchy to retrieve valid hierarchy.
-   */
+    */
   lazy val byName: Map[String, T] = values.iterator.map(v => (v.name, v)).toMap
 
   implicit lazy val keyCodec: GenKeyCodec[T] = GenKeyCodec.create(byName, _.name)
   implicit lazy val codec: GenCodec[T] = GenCodec.fromKeyCodec
+}
+
+/**
+  * Trait to be extended by enums whose values are ordered by declaration order. Ordering is derived from
+  * [[SourceInfo]] object, which is typically accepted as an implicit, e.g.
+  *
+  * {{{
+  *   sealed abstract class MyOrderedEnum(implicit val sourceInfo: SourceInfo) extends OrderedEnum
+  *   object MyOrderedEnum {
+  *     case object First extends MyOrderedEnum
+  *     case object Second extends MyOrderedEnum
+  *     case object Third extends MyOrderedEnum
+  *
+  *     val values: List[MyOrderedEnum] = caseObjects
+  *   }
+  * }}}
+  *
+  * In the example above, `values` is guaranteed to return `First`, `Second` and `Third` objects in exactly that order.
+  */
+trait OrderedEnum extends Any {
+  def sourceInfo: SourceInfo
+}
+object OrderedEnum {
+  private object reusableOrdering extends Ordering[OrderedEnum] {
+    def compare(x: OrderedEnum, y: OrderedEnum) = Integer.compare(x.sourceInfo.offset, y.sourceInfo.offset)
+  }
+  implicit def ordering[T <: OrderedEnum]: Ordering[T] =
+    reusableOrdering.asInstanceOf[Ordering[T]]
 }
