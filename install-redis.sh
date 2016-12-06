7667864fa478f423ed352436d58cd8acba060d8f
ghik <romeqjanoosh@gmail.com>
2016-12-06 10:27:50 +0100

    redis 4.0-rc2

--- install-redis.sh
+++ install-redis.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ ! -d $REDIS_HOME ]; then
-    wget http://download.redis.io/releases/redis-$REDIS_VERSION.tar.gz
+    wget https://github.com/antirez/redis/archive/$REDIS_VERSION.tar.gz
     tar -xzf redis-$REDIS_VERSION.tar.gz
     cd redis-$REDIS_VERSION && make
 fi
