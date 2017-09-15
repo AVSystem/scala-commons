0c27fd92b75a1e13bb8475f54d9ba80de704f16b
Michał Kuleta <m.kuleta@avsystem.com>
2017-09-15 16:29:55 +0200

    Now using "case" instead of "type", renamed marker field to "@case"

--- commons-shared/src/main/scala/com/avsystem/commons/serialization/PolymorphicGenCodec.scala
+++ commons-shared/src/main/scala/com/avsystem/commons/serialization/PolymorphicGenCodec.scala
@@ -5,20 +5,20 @@ import com.avsystem.commons.serialization.GenCodec.NullSafeCodec
 import com.avsystem.commons.serialization.PolymorphicGenCodec.Variant
 
 class PolymorphicGenCodec[T](variants: Variant[_ <: T]*) extends NullSafeCodec[T] {
-  private val nameToVariant = variants.iterator.mkMap(_.name, identity)
+  private val nameToVariant = variants.iterator.mkMap(_.caseName, identity)
 
-  private lazy val supportedTypesDebug = variants.map { v =>
-    s"${v.name} -> ${v.ct.runtimeClass.getName}"
+  private lazy val supportedCasesDebug = variants.map { v =>
+    s"${v.caseName} -> ${v.ct.runtimeClass.getName}"
   }.mkString(", ")
 
   override protected def nullable: Boolean = true
 
   override protected def readNonNull(input: Input): T = {
     val objectInput = input.readObject()
-    val tpe = objectInput.nextField().assertField(PolymorphicGenCodec.ObjectTypeField).readString()
+    val cse = objectInput.nextField().assertField(PolymorphicGenCodec.CaseField).readString()
     val variant = nameToVariant.getOrElse(
-      tpe,
-      throw new IllegalArgumentException(s"Unsupported type marker: $tpe. Supported types are: $supportedTypesDebug")
+      cse,
+      throw new IllegalArgumentException(s"Unsupported case marker: $cse. Supported cases are: $supportedCasesDebug")
     )
     variant.readFrom(objectInput)
   }
@@ -26,43 +26,45 @@ class PolymorphicGenCodec[T](variants: Variant[_ <: T]*) extends NullSafeCodec[T
   override protected def writeNonNull(output: Output, value: T): Unit = {
     val objectOutput = output.writeObject()
     variants.find(_.maybeWriteTo(objectOutput, value)).getOrElse {
-      throw new IllegalArgumentException(s"Unsupported object: $value of type ${value.getClass.getName}. Supported types are: $supportedTypesDebug")
+      throw new IllegalArgumentException(s"Unsupported object: $value of type ${value.getClass.getName}. Supported cases are: $supportedCasesDebug")
     }
   }
 }
 
 object PolymorphicGenCodec {
-  private class CustomObjectInput(objectInput: ObjectInput) extends Input {
-    override def inputType: InputType = InputType.Object
-    override def readNull(): Null = throw new UnsupportedOperationException
-    override def readString(): String = throw new UnsupportedOperationException
-    override def readBoolean(): Boolean = throw new UnsupportedOperationException
-    override def readInt(): Int = throw new UnsupportedOperationException
-    override def readLong(): Long = throw new UnsupportedOperationException
-    override def readDouble(): Double = throw new UnsupportedOperationException
-    override def readBinary(): Array[Byte] = throw new UnsupportedOperationException
-    override def readList(): ListInput = throw new UnsupportedOperationException
-    override def skip(): Unit = throw new UnsupportedOperationException
+  private def unsupported = throw new UnsupportedOperationException
 
+  private class CustomObjectInput(objectInput: ObjectInput) extends Input {
+    override def readNull(): Null = unsupported
+    override def readString(): String = unsupported
+    override def readBoolean(): Boolean = unsupported
+    override def readInt(): Int = unsupported
+    override def readLong(): Long = unsupported
+    override def readDouble(): Double = unsupported
+    override def readBinary(): Array[Byte] = unsupported
+    override def readList(): ListInput = unsupported
+    override def skip(): Unit = unsupported
+
+    override def inputType: InputType = InputType.Object
     override def readObject(): ObjectInput = objectInput
   }
 
   private class CustomObjectOutput(objectOutput: ObjectOutput) extends Output {
-    override def writeNull(): Unit = throw new UnsupportedOperationException
-    override def writeString(str: String): Unit = throw new UnsupportedOperationException
-    override def writeBoolean(boolean: Boolean): Unit = throw new UnsupportedOperationException
-    override def writeInt(int: Int): Unit = throw new UnsupportedOperationException
-    override def writeLong(long: Long): Unit = throw new UnsupportedOperationException
-    override def writeDouble(double: Double): Unit = throw new UnsupportedOperationException
-    override def writeBinary(binary: Array[Byte]): Unit = throw new UnsupportedOperationException
-    override def writeList(): ListOutput = throw new UnsupportedOperationException
+    override def writeNull(): Unit = unsupported
+    override def writeString(str: String): Unit = unsupported
+    override def writeBoolean(boolean: Boolean): Unit = unsupported
+    override def writeInt(int: Int): Unit = unsupported
+    override def writeLong(long: Long): Unit = unsupported
+    override def writeDouble(double: Double): Unit = unsupported
+    override def writeBinary(binary: Array[Byte]): Unit = unsupported
+    override def writeList(): ListOutput = unsupported
 
     override def writeObject(): ObjectOutput = objectOutput
   }
 
-  val ObjectTypeField = "objType"
+  val CaseField = "@case"
 
-  case class Variant[T](name: String)(implicit val ct: ClassTag[T], val codec: GenCodec[T]) {
+  case class Variant[T](caseName: String)(implicit val ct: ClassTag[T], val codec: GenCodec[T]) {
     def readFrom(objectInput: ObjectInput): T = {
       codec.read(new CustomObjectInput(objectInput))
     }
@@ -70,7 +72,7 @@ object PolymorphicGenCodec {
     def maybeWriteTo[U >: T](objectOutput: ObjectOutput, value: U): Boolean = {
       value match {
         case ct(t) =>
-          objectOutput.writeField(ObjectTypeField).writeString(name)
+          objectOutput.writeField(CaseField).writeString(caseName)
           codec.write(new CustomObjectOutput(objectOutput), t)
           true
         case _ =>
