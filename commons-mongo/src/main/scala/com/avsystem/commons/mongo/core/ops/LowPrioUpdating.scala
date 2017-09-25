451c0d83a1899be07f64fc9ffeb285a5819fc394
Michał Kuleta <m.kuleta@avsystem.com>
2017-09-25 10:33:08 +0200

    commons-mongo: added missing implicit in LowPrioUpdating

--- commons-mongo/src/main/scala/com/avsystem/commons/mongo/core/ops/LowPrioUpdating.scala
+++ commons-mongo/src/main/scala/com/avsystem/commons/mongo/core/ops/LowPrioUpdating.scala
@@ -4,5 +4,5 @@ package mongo.core.ops
 import com.avsystem.commons.mongo.{BsonRef, DocKey}
 
 trait LowPrioUpdating {
-  def bsonRefUpdating[T](bsonRef: BsonRef[T]): BsonRefUpdating[T] = new BsonRefUpdating(bsonRef)
+  implicit def bsonRefUpdating[T](bsonRef: BsonRef[T]): BsonRefUpdating[T] = new BsonRefUpdating(bsonRef)
 }
