4cd20b6c2dd7ed5cbce6e33626d0da1da705f546
Michał Kuleta <m.kuleta@avsystem.com>
2017-09-29 11:38:53 +0200

    commons-mongo: added GenCodecProvider for proper subclass codec handling

---
+++ commons-mongo/src/main/scala/com/avsystem/commons/mongo/core/GenCodecProvider.scala
@@ -0,0 +1,17 @@
+package com.avsystem.commons
+package mongo.core
+
+import com.avsystem.commons.mongo.GenCodecBasedBsonCodec
+import com.avsystem.commons.serialization.GenCodec
+import org.bson.codecs.Codec
+import org.bson.codecs.configuration.{CodecProvider, CodecRegistry}
+
+class GenCodecProvider[T: GenCodec](implicit ct: ClassTag[T]) extends CodecProvider {
+  private val mongoCodec = new GenCodecBasedBsonCodec[T]()
+  private val runtimeClass = ct.runtimeClass
+
+  override def get[X](clazz: Class[X], registry: CodecRegistry): Codec[X] = {
+    if (runtimeClass.isAssignableFrom(clazz)) mongoCodec.asInstanceOf[Codec[X]]
+    else null
+  }
+}
