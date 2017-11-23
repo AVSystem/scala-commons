3901923037864b12c046dfb429bfb49f0613758d
ghik <romeqjanoosh@gmail.com>
2017-11-23 11:49:25 +0100

    CaseMethodsTest

---
+++ commons-shared/src/test/scala/com/avsystem/commons/misc/CaseMethodsTest.scala
@@ -0,0 +1,32 @@
+package com.avsystem.commons
+package misc
+
+import org.scalatest.FunSuite
+
+case class Whatever(str: String, int: Int) extends AbstractCase
+
+class CaseMethodsTest extends FunSuite {
+  val first = Whatever("lol", 42)
+  val second = Whatever("lol", 42)
+  val other = Whatever("lol", 41)
+
+  test("equality") {
+    assert(first canEqual second)
+    assert(first == second)
+    assert(first equals second)
+  }
+
+  test("inequality") {
+    assert(first != other)
+    assert(!(first equals other))
+  }
+
+  test("hashCode") {
+    assert(first.hashCode == second.hashCode)
+    assert(first.## == second.##)
+  }
+
+  test("toString") {
+    assert(first.toString == "Whatever(lol,42)")
+  }
+}
