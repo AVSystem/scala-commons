08d7b865b8614bf55e36c11f858c62264d8cbb52
ghik <romeqjanoosh@gmail.com>
2017-11-29 13:00:01 +0100

    simple custom mutable stack implementation

---
+++ commons-shared/src/main/scala/com/avsystem/commons/collection/MutableStack.scala
@@ -0,0 +1,61 @@
+package com.avsystem.commons
+package collection
+
+final class MutableStack[T] {
+  private[this] var ssize: Int = 0
+  private[this] var stack = List.empty[T]
+
+  def push(elem: T): Unit = {
+    stack ::= elem
+    ssize += 1
+  }
+
+  def pushAll(elems: TraversableOnce[T]): Unit =
+    elems.foreach(push)
+
+  def pop(): T = stack match {
+    case head :: tail =>
+      stack = tail
+      ssize -= 1
+      head
+    case Nil => throw new NoSuchElementException("pop on empty stack")
+  }
+
+  def popOpt(): Opt[T] = stack match {
+    case head :: tail =>
+      stack = tail
+      ssize -= 1
+      Opt.some(head)
+    case Nil => Opt.Empty
+  }
+
+  def popOption(): Option[T] = stack match {
+    case head :: tail =>
+      stack = tail
+      ssize -= 1
+      Some(head)
+    case Nil => None
+  }
+
+  def top: T = stack match {
+    case head :: _ => head
+    case _ => throw new NoSuchElementException("top on empty stack")
+  }
+
+  def topOpt: Opt[T] = stack match {
+    case head :: _ => Opt.some(head)
+    case _ => Opt.Empty
+  }
+
+  def topOption: Option[T] =
+    stack.headOption
+
+  def asList: List[T] =
+    stack
+
+  def size: Int =
+    ssize
+
+  def isEmpty: Boolean =
+    stack.isEmpty
+}
---
+++ commons-shared/src/test/scala/com/avsystem/commons/collection/MutableStackTest.scala
@@ -0,0 +1,48 @@
+package com.avsystem.commons
+package collection
+
+import org.scalatest.FunSuite
+
+class MutableStackTest extends FunSuite {
+  test("push") {
+    val stack = new MutableStack[String]
+    stack.push("lol1")
+    stack.push("lol2")
+    stack.push("lol3")
+    assert(stack.asList == List("lol3", "lol2", "lol1"))
+  }
+
+  test("pushAll") {
+    val stack = new MutableStack[String]
+    stack.pushAll(List("lol1", "lol2", "lol3"))
+    assert(stack.asList == List("lol3", "lol2", "lol1"))
+  }
+
+  test("pop") {
+    val stack = new MutableStack[String]
+    assertThrows[NoSuchElementException](stack.pop())
+    assert(stack.popOpt().isEmpty)
+    assert(stack.popOption().isEmpty)
+    stack.push("lol")
+    assert(stack.pop() == "lol")
+    assert(stack.isEmpty)
+    stack.push("lol")
+    assert(stack.popOpt() == Opt("lol"))
+    assert(stack.isEmpty)
+    stack.push("lol")
+    assert(stack.popOption().contains("lol"))
+    assert(stack.isEmpty)
+  }
+
+  test("top") {
+    val stack = new MutableStack[String]
+    assertThrows[NoSuchElementException](stack.top)
+    assert(stack.topOpt.isEmpty)
+    assert(stack.topOption.isEmpty)
+    stack.push("lol")
+    assert(stack.top == "lol")
+    assert(stack.topOpt.contains("lol"))
+    assert(stack.topOption.contains("lol"))
+  }
+
+}
