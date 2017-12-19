367b3499cc69832b964dc965459ef2dd4881bbc1
ghik <romeqjanoosh@gmail.com>
2017-12-19 08:23:32 +0100

    mongoId scaladoc

--- commons-mongo/src/main/scala/com/avsystem/commons/mongo/mongoId.scala
+++ commons-mongo/src/main/scala/com/avsystem/commons/mongo/mongoId.scala
@@ -4,5 +4,10 @@ package mongo
 import com.avsystem.commons.annotation.AnnotationAggregate
 import com.avsystem.commons.serialization.{name, outOfOrder}
 
-@name("_id") @outOfOrder
+/**
+  * Shortcut annotation for applying `@name("_id")` and `@outOfOrder` annotation on a case class field,
+  * which is typical for MongoDB ID field.
+  */
+@name("_id")
+@outOfOrder
 class mongoId extends AnnotationAggregate
