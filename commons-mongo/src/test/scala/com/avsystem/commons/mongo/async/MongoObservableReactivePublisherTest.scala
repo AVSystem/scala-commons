c1a1fb900f7e8178d108c81d0e32b32d40fa74f5
ddworak <dawid.dworak@gmail.com>
2018-04-10 16:49:17 +0200

    Mongo -> Monix basic tests

--- commons-mongo/src/test/scala/com/avsystem/commons/mongo/async/MongoObservableReactivePublisherTest.scala
+++ commons-mongo/src/test/scala/com/avsystem/commons/mongo/async/MongoObservableReactivePublisherTest.scala
@@ -1,66 +1,86 @@
 package com.avsystem.commons
 package mongo.async
 
-import com.mongodb.async.client.Observer
+import com.avsystem.commons.concurrent.RunNowEC
+import com.mongodb.async.{client => mongo}
+import monix.execution.{Cancelable, Scheduler}
 import org.mockito.ArgumentMatchers.{eq => eqTo, _}
 import org.mockito.Mockito._
 import org.mongodb.scala.{Completed, Document, FindObservable, MongoCollection, SingleObservable}
-import org.scalatest.FunSuite
+import org.scalactic.source.Position
+import org.scalatest.FreeSpec
 import org.scalatest.mockito.MockitoSugar
 
 import scala.concurrent.duration.Duration
 
-class MongoObservableReactivePublisherTest extends FunSuite with MockitoSugar {
+class MongoObservableReactivePublisherTest extends FreeSpec with MockitoSugar {
 
-  test("Dropping test collection") {
-    val collection: MongoCollection[Document] = mock[MongoCollection[Document]]
-    when(collection.drop()).thenReturn(SingleObservable(Completed()))
-    val dropSubscriber = TestSubscriber[Completed]()
+  abstract class MockedObservableTests(implicit position: Position) {
 
-    collection.drop().asReactive.subscribe(dropSubscriber)
+    def subscribe[T](obs: mongo.Observable[T], testSubscriber: TestSubscriber[T]): Unit
 
-    dropSubscriber.assertNoTerminalEvent()
-    dropSubscriber.requestMore(1)
-    dropSubscriber.awaitTerminalEvent(Duration(100, "ms"))
-    dropSubscriber.assertNoErrors()
-    dropSubscriber.assertReceivedOnNext(Seq(Completed()))
+    "should drop test collection" in {
+      val collection: MongoCollection[Document] = mock[MongoCollection[Document]]
+      when(collection.drop()).thenReturn(SingleObservable(Completed()))
+      val dropSubscriber = TestSubscriber[Completed]()
 
-    verify(collection).drop()
-    verifyNoMoreInteractions(collection)
+      subscribe(collection.drop(), dropSubscriber)
+
+      dropSubscriber.assertNoTerminalEvent()
+      dropSubscriber.requestMore(1)
+      dropSubscriber.awaitTerminalEvent(Duration(100, "ms"))
+      dropSubscriber.assertNoErrors()
+      dropSubscriber.assertReceivedOnNext(Seq(Completed()))
+
+      verify(collection).drop()
+      verifyNoMoreInteractions(collection)
+    }
+
+    "should insert documents" in {
+      val collection: MongoCollection[Document] = mock[MongoCollection[Document]]
+      val insertSubscriber = TestSubscriber[Completed]()
+      when(collection.insertMany(any())).thenReturn(SingleObservable(Completed()))
+
+      val documents: IndexedSeq[Document] = (1 to 100) map { i: Int => Document("_id" -> i) }
+      subscribe(collection.insertMany(documents), insertSubscriber)
+      insertSubscriber.requestMore(1)
+      insertSubscriber.awaitTerminalEvent(Duration(100, "ms"))
+      insertSubscriber.assertNoErrors()
+      insertSubscriber.assertReceivedOnNext(Seq(Completed()))
+
+      verify(collection).insertMany(eqTo(documents))
+      verifyNoMoreInteractions(collection)
+    }
+
+    "should find documents" in {
+      val documents: IndexedSeq[Document] = (1 to 100) map { i: Int => Document("_id" -> i) }
+      val original = mock[FindObservable[Document]]
+      val findSubscriber = TestSubscriber[Document]()
+      doNothing().when(original).subscribe(any())
+
+      subscribe(original, findSubscriber)
+      findSubscriber.assertNoTerminalEvent()
+      findSubscriber.requestMore(101)
+      documents.foreach(findSubscriber.onNext)
+      findSubscriber.onComplete()
+      findSubscriber.awaitTerminalEvent(Duration(100, "ms"))
+      findSubscriber.assertNoErrors()
+      findSubscriber.assertReceivedOnNext(documents)
+
+
+      verify(original).subscribe(any(classOf[mongo.Observer[_ >: Document]]))
+      verifyNoMoreInteractions(original)
+    }
   }
 
-  test("Inserting and finding documents") {
-    val collection: MongoCollection[Document] = mock[MongoCollection[Document]]
-    val insertSubscriber = TestSubscriber[Completed]()
-    when(collection.insertMany(any())).thenReturn(SingleObservable(Completed()))
-
-    val documents: IndexedSeq[Document] = (1 to 100) map { i: Int => Document("_id" -> i) }
-    collection.insertMany(documents).asReactive.subscribe(insertSubscriber)
-    insertSubscriber.requestMore(1)
-    insertSubscriber.awaitTerminalEvent(Duration(10, "s"))
-    insertSubscriber.assertNoErrors()
-    insertSubscriber.assertReceivedOnNext(Seq(Completed()))
-
-    verify(collection).insertMany(eqTo(documents))
-    verifyNoMoreInteractions(collection)
-
-    val original = mock[FindObservable[Document]]
-    val findSubscriber = TestSubscriber[Document]()
-    doNothing().when(original).subscribe(any())
-
-    val reactive = original.asReactive
-    reactive.subscribe(findSubscriber)
-    findSubscriber.assertNoTerminalEvent()
-    findSubscriber.requestMore(101)
-    documents.foreach(findSubscriber.onNext)
-    findSubscriber.onComplete()
-    findSubscriber.awaitTerminalEvent(Duration(100, "ms"))
-    findSubscriber.assertNoErrors()
-    findSubscriber.assertReceivedOnNext(documents)
-
-
-    verify(original).subscribe(any(classOf[Observer[_ >: Document]]))
-    verifyNoMoreInteractions(original)
+  "A Mongo-Reactive observable" - new MockedObservableTests {
+    override def subscribe[T](obs: mongo.Observable[T], testSubscriber: TestSubscriber[T]): Unit =
+      obs.asReactive.subscribe(testSubscriber)
   }
-
+  "A Mongo-Monix observable" - new MockedObservableTests {
+    override def subscribe[T](obs: mongo.Observable[T], testSubscriber: TestSubscriber[T]): Unit =
+      obs.asMonix.subscribe(
+        monix.reactive.observers.Subscriber.fromReactiveSubscriber(testSubscriber, Cancelable.empty)(Scheduler(RunNowEC))
+      )
+  }
 }
