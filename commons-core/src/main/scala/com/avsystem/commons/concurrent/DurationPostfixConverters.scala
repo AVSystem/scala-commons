36bd16866eacee6de14a674ba8111d848a287d3d
ghik <romeqjanoosh@gmail.com>
2018-04-10 13:48:15 +0200

    fixed ambiguous implicits

--- commons-core/src/main/scala/com/avsystem/commons/concurrent/DurationPostfixConverters.scala
+++ commons-core/src/main/scala/com/avsystem/commons/concurrent/DurationPostfixConverters.scala
@@ -4,7 +4,7 @@ package concurrent
 import scala.concurrent.duration._
 
 trait DurationPostfixConverters {
-  implicit def durationInt(int: Int): DurationInt = int
-  implicit def durationLong(long: Long): DurationLong = long
-  implicit def durationLong(double: Double): DurationDouble = double
+  implicit def durationInt(int: Int): DurationInt = new DurationInt(int)
+  implicit def durationLong(long: Long): DurationLong = new DurationLong(long)
+  implicit def durationDouble(double: Double): DurationDouble = new DurationDouble(double)
 }
