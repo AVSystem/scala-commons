26f6a71ebc18bd20185f1b31c7678181b13d34c6
ghik <romeqjanoosh@gmail.com>
2018-06-13 17:01:39 +0200

    moved zipWithNow to scala-2.12 code

--- commons-core/src/main/scala-2.12/com/avsystem/commons/CompatSharedExtensions.scala
+++ commons-core/src/main/scala-2.12/com/avsystem/commons/CompatSharedExtensions.scala
@@ -1,6 +1,7 @@
 package com.avsystem.commons
 
 import com.avsystem.commons.CompatSharedExtensions.FutureCompatOps
+import com.avsystem.commons.concurrent.RunNowEC
 
 trait CompatSharedExtensions {
   implicit def futureCompatOps[A](fut: Future[A]): FutureCompatOps[A] = new FutureCompatOps(fut)
@@ -9,5 +10,8 @@ trait CompatSharedExtensions {
 object CompatSharedExtensions {
   final class FutureCompatOps[A](private val fut: Future[A]) extends AnyVal {
     def transformTry[S](f: Try[A] => Try[S])(implicit ec: ExecutionContext): Future[S] = fut.transform(f)
+
+    def zipWithNow[B, R](that: Future[B])(f: (A, B) => R): Future[R] =
+      fut.zipWith(that)(f)(RunNowEC)
   }
 }
--- commons-core/src/main/scala/com/avsystem/commons/SharedExtensions.scala
+++ commons-core/src/main/scala/com/avsystem/commons/SharedExtensions.scala
@@ -238,9 +238,6 @@ object SharedExtensions extends SharedExtensions {
     def recoverWithNow[B >: A](pf: PartialFunction[Throwable, Future[B]]): Future[B] =
       fut.recoverWith(pf)(RunNowEC)
 
-    def zipWithNow[B, R](that: Future[B])(f: (A, B) => R): Future[R] =
-      fut.zipWith(that)(f)(RunNowEC)
-
     def toUnit: Future[Unit] =
       mapNow(_ => ())
 
