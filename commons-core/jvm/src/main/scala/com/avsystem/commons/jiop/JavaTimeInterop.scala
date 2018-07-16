19ab22ba3188eb4a1ebfe2805a92cd8a3115e638
ghik <romeqjanoosh@gmail.com>
2018-07-16 11:41:04 +0200

    more review changes

--- commons-core/jvm/src/main/scala/com/avsystem/commons/jiop/JavaTimeInterop.scala
+++ commons-core/jvm/src/main/scala/com/avsystem/commons/jiop/JavaTimeInterop.scala
@@ -3,9 +3,15 @@ package jiop
 
 import java.time.Instant
 
-import com.avsystem.commons.misc.TimestampConversions
+import com.avsystem.commons.jiop.JavaTimeInterop.InstantOps
+import com.avsystem.commons.misc.Timestamp
 
 trait JavaTimeInterop {
-  implicit def instantTimestampConversions(instant: Instant): TimestampConversions =
-    new TimestampConversions(instant.toEpochMilli)
+  implicit def instantOps(instant: Instant): InstantOps = new InstantOps(instant)
+}
+object JavaTimeInterop {
+  class InstantOps(private val instant: Instant) extends AnyVal {
+    def truncateToTimestamp: Timestamp = Timestamp(instant.toEpochMilli)
+    def truncateToJDate: JDate = new JDate(instant.toEpochMilli)
+  }
 }
