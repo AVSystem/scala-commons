8a68c433816f7736f0e5c16ed437d1887a1c2110
ghik <romeqjanoosh@gmail.com>
2018-10-02 14:28:35 +0200

    doc fix

--- commons-core/src/main/scala/com/avsystem/commons/meta/Fallback.scala
+++ commons-core/src/main/scala/com/avsystem/commons/meta/Fallback.scala
@@ -1,11 +1,9 @@
 package com.avsystem.commons
 package meta
 
-import com.avsystem.commons.rpc.RpcMetadataCompanion
-
 /**
   * Wrap your implicit instance of `AsReal`, `AsRaw`, `AsRawReal` or RPC metadata (with companion that extends
-  * [[RpcMetadataCompanion]] into Fallback` in order to lower its implicit priority.
+  * `RpcMetadataCompanion` into Fallback` in order to lower its implicit priority.
   * Useful when some implicit must be imported but we don't want it to get higher priority that imports normally
   * have over implicit scope (e.g. implicits from companion objects).
   *
