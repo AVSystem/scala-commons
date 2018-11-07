96f29a0fce2da37972a998d3902e73cdd9d4a32d
ghik <romeqjanoosh@gmail.com>
2018-11-07 14:57:07 +0100

    base companions for materializing codecs with injectable implicit deps

--- commons-core/src/main/scala/com/avsystem/commons/serialization/HasGenCodec.scala
+++ commons-core/src/main/scala/com/avsystem/commons/serialization/HasGenCodec.scala
@@ -2,6 +2,7 @@ package com.avsystem.commons
 package serialization
 
 import com.avsystem.commons.meta.MacroInstances
+import com.avsystem.commons.misc.ValueOf
 
 /**
   * Convenience abstract class for companion objects of types that have a [[GenCodec]].
@@ -16,4 +17,16 @@ abstract class HasApplyUnapplyCodec[T](implicit macroCodec: MacroInstances[Unit,
 
 abstract class HasGenObjectCodec[T](implicit macroCodec: MacroInstances[Unit, () => GenObjectCodec[T]]) {
   implicit val codec: GenObjectCodec[T] = macroCodec((), this).apply()
+}
+
+abstract class HasGenCodecWithDeps[D, T](implicit deps: ValueOf[D], macroCodec: MacroInstances[D, () => GenCodec[T]]) {
+  implicit val codec: GenCodec[T] = macroCodec(deps.value, this).apply()
+}
+
+abstract class HasApplyUnapplyCodecWithDeps[D, T](implicit deps: ValueOf[D], macroCodec: MacroInstances[D, () => ApplyUnapplyCodec[T]]) {
+  implicit val codec: ApplyUnapplyCodec[T] = macroCodec(deps.value, this).apply()
+}
+
+abstract class HasGenObjectCodecWithDeps[D, T](implicit deps: ValueOf[D], macroCodec: MacroInstances[D, () => GenObjectCodec[T]]) {
+  implicit val codec: GenObjectCodec[T] = macroCodec(deps.value, this).apply()
 }
