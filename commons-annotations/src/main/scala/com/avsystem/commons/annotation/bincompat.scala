0ebd9f146dc5095eea558a59c62dfe8912880893
ghik <romeqjanoosh@gmail.com>
2018-11-14 13:19:01 +0100

    forwarder methods for binary compatibility

---
+++ commons-annotations/src/main/scala/com/avsystem/commons/annotation/bincompat.scala
@@ -0,0 +1,9 @@
+package com.avsystem.commons
+package annotation
+
+import scala.annotation.Annotation
+
+/**
+  * Annotation to put on symbols which exist only for binary compatibility with previous versions.
+  */
+class bincompat extends Annotation
