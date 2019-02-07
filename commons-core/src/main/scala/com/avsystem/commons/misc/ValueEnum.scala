a8e3e27e9579d0b48c9aac6854f5ab8463b6a175
ghik <romeqjanoosh@gmail.com>
2019-02-07 12:49:52 +0100

    refactored Ordering/Ordered implicits for ValueEnum

--- commons-core/src/main/scala/com/avsystem/commons/misc/ValueEnum.scala
+++ commons-core/src/main/scala/com/avsystem/commons/misc/ValueEnum.scala
@@ -73,11 +73,6 @@ trait ValueEnum extends NamedEnum {
   */
 abstract class AbstractValueEnum(protected implicit val enumCtx: EnumCtx) extends ValueEnum
 
-object ValueEnum {
-  private[this] val reusableOrdering: Ordering[ValueEnum] = Ordering.by(_.ordinal)
-  implicit def ordering[T <: ValueEnum]: Ordering[T] = reusableOrdering.asInstanceOf[Ordering[T]]
-}
-
 @implicitNotFound("Value based enum must be assigned to a public, final, non-lazy val in its companion object " +
   "with explicit `Value` type ascribed, e.g. `final val Monday: Value = new Weekday")
 sealed trait EnumCtx extends Any {
@@ -109,6 +104,9 @@ trait ValueEnumCompanion[T <: ValueEnum] extends NamedEnumCompanion[T] { compani
     finished = true
     registryBuilder.result()
   }
+
+  implicit final val ordering: Ordering[T] = Ordering.by(_.ordinal)
+  implicit final def ordered(value: T): Ordered[T] = Ordered.orderingToOrdered(value)
 
   private class Ctx(val valName: String, val ordinal: Int) extends EnumCtx {
     if (awaitingRegister) {
