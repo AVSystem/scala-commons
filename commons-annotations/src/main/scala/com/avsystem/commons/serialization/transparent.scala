390d662ee246860053ae9a211b60fa7246d67b2e
ghik <romeqjanoosh@gmail.com>
2019-03-20 11:59:44 +0100

    note about TransparentWrapperCompanion in transparent annotation doc

--- commons-annotations/src/main/scala/com/avsystem/commons/serialization/transparent.scala
+++ commons-annotations/src/main/scala/com/avsystem/commons/serialization/transparent.scala
@@ -7,5 +7,8 @@ import scala.annotation.StaticAnnotation
   * Can be used on case classes with exactly one field to instruct automatically generated `GenCodec` that the
   * class is a "transparent wrapper" and should be serialized to the same representation as the value of its sole
   * field.
+  *
+  * NOTE: `TransparentWrapperCompanion` is another and even better way of achieving the same thing.
+  * (`@transparent` annotation will probably become deprecated).
   */
 class transparent extends StaticAnnotation
