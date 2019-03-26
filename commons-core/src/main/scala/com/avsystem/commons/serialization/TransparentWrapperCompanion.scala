7e29ba063a2c473b8bc4edb1baf02803f41724b2
ghik <romeqjanoosh@gmail.com>
2019-03-26 10:24:00 +0100

    removed bincompat API

--- commons-core/src/main/scala/com/avsystem/commons/serialization/TransparentWrapperCompanion.scala
+++ commons-core/src/main/scala/com/avsystem/commons/serialization/TransparentWrapperCompanion.scala
@@ -1,8 +1,6 @@
 package com.avsystem.commons
 package serialization
 
-import com.avsystem.commons.annotation.bincompat
-
 /**
   * A typeclass which serves as evidence that some type `T` is a "transparent" wrapper of some other type.
   * This usually means that instances of various typeclasses (e.g. [[GenCodec]]) for type `T` could be automatically
@@ -20,13 +18,6 @@ trait TransparentWrapping[R, T] {
   * [[transparent]] annotation.
   */
 abstract class TransparentWrapperCompanion[R, T] extends TransparentWrapping[R, T] with (R => T) {
-  @bincompat private[this] var transformedCodec: GenCodec[T] = _
-
-  @bincompat private[commons] def this(wrappedCodec: GenCodec[R]) = {
-    this()
-    transformedCodec = new GenCodec.Transformed(wrappedCodec, unwrap, wrap)
-  }
-
   implicit def self: this.type = this
 
   def apply(r: R): T
@@ -34,8 +25,6 @@ abstract class TransparentWrapperCompanion[R, T] extends TransparentWrapping[R, 
 
   final def wrap(r: R): T = apply(r)
   final def unwrap(t: T): R = unapply(t).getOrElse(throw new NoSuchElementException(s"unapply for $t failed"))
-
-  @bincompat private[commons] def codec: GenCodec[T] = transformedCodec
 }
 
 abstract class StringWrapperCompanion[T] extends TransparentWrapperCompanion[String, T]
