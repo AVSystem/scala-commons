cf1f92cb07a06be5169786129534e0de5b3a8f64
ddworak <dawid.dworak@gmail.com>
2019-05-28 10:54:24 +0200

    Support varargs in Delegation

--- commons-core/src/test/scala/com/avsystem/commons/misc/DelegationTest.scala
+++ commons-core/src/test/scala/com/avsystem/commons/misc/DelegationTest.scala
@@ -10,12 +10,16 @@ class DelegationTest extends FunSuite {
     val te: T
     def simple(omg: Int): String
     def meth[C[+X] >: Null <: Traversable[X]](map: Map[T, C[String]]): C[(T, String)]
+    def multi(a: String)(b: String): String
+    def vararg(values: String*): String
   }
 
   class Source {
     val te: Double = 3.14
     def simple(omg: Int): String = omg.toString
     def meth[C[+X] >: Null <: Traversable[X]](map: Map[Double, C[String]]): C[(Double, String)] = null
+    def multi(a: String)(b: String): String = a + b
+    def vararg(values: String*): String = values.mkString("")
   }
 
   test("simple test") {
@@ -25,5 +29,7 @@ class DelegationTest extends FunSuite {
     assert(source.te == destination.te)
     assert(source.simple(42) == destination.simple(42))
     assert(source.meth[List](Map.empty) == destination.meth[List](Map.empty))
+    assert(source.multi("4")("2") == destination.multi("4")("2"))
+    assert(source.vararg("4", "2") == destination.vararg("4", "2"))
   }
 }
