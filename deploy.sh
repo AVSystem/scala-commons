a9e504834f39e21385d3bad136d5f201ad674963
ghik <romeqjanoosh@gmail.com>
2019-05-30 17:42:06 +0200

    fixed path to secrets file

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
-tar xv -C travis -f travis/secrets.tar
+openssl aes-256-cbc -K $encrypted_b21e52cd6f46_key -iv $encrypted_b21e52cd6f46_iv -in travis/secrets.tar.enc -out travis/local.secrets.tar -d
+tar xv -C travis -f travis/local.secrets.tar
 sbt +publishSigned sonatypeRelease
