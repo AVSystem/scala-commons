284cb3a961e063dff63571b68b22c52d06ec2da1
ghik <romeqjanoosh@gmail.com>
2019-10-23 18:13:54 +0200

    cosmetic

--- commons-core/src/main/scala/com/avsystem/commons/meta/MacroInstances.scala
+++ commons-core/src/main/scala/com/avsystem/commons/meta/MacroInstances.scala
@@ -58,7 +58,7 @@ object MacroInstances {
     *   trait SomeRawRpc { ... }
     *   class SomeMetadata[Real](...)
     *
-    *   trait SomeInstances[Real} {
+    *   trait SomeInstances[Real] {
     *     def asReal: AsReal[SomeRawRpc, Real]
     *     def metadata: SomeMetadata[Real]
     *   }
