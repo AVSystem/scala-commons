2f9c024aa4ca15d9ee5dc2d5e25ffde1071b8047
ddworak <dawid.dworak@gmail.com>
2020-03-10 08:17:10 +0100

    Fix tests in Java 11
    Used Date for reflection test to avoid API differences.

--- commons-core/src/test/scala/com/avsystem/commons/rpc/ApiReflectionTest.scala
+++ commons-core/src/test/scala/com/avsystem/commons/rpc/ApiReflectionTest.scala
@@ -75,61 +75,31 @@ class SimpleApi {
 }
 
 class ApiReflectionTest extends AnyFunSuite {
-  test("String API") {
-    assert(ApiInfo.materialize[String].repr ==
-      """String {
-        |  def length(): Int
-        |  def isEmpty(): Boolean
-        |  def charAt(x$1: Int): Char
-        |  def codePointAt(x$1: Int): Int
-        |  def codePointBefore(x$1: Int): Int
-        |  def codePointCount(x$1: Int, x$2: Int): Int
-        |  def offsetByCodePoints(x$1: Int, x$2: Int): Int
-        |  def getChars(x$1: Int, x$2: Int, x$3: Array[Char], x$4: Int): Unit
-        |  def getBytes(x$1: Int, x$2: Int, x$3: Array[Byte], x$4: Int): Unit
-        |  def getBytes(x$1: String): Array[Byte]
-        |  def getBytes(x$1: java.nio.charset.Charset): Array[Byte]
-        |  def getBytes(): Array[Byte]
-        |  def contentEquals(x$1: StringBuffer): Boolean
-        |  def contentEquals(x$1: CharSequence): Boolean
-        |  def equalsIgnoreCase(x$1: String): Boolean
-        |  def compareTo(x$1: String): Int
-        |  def compareToIgnoreCase(x$1: String): Int
-        |  def regionMatches(x$1: Int, x$2: String, x$3: Int, x$4: Int): Boolean
-        |  def regionMatches(x$1: Boolean, x$2: Int, x$3: String, x$4: Int, x$5: Int): Boolean
-        |  def startsWith(x$1: String, x$2: Int): Boolean
-        |  def startsWith(x$1: String): Boolean
-        |  def endsWith(x$1: String): Boolean
-        |  def indexOf(x$1: Int): Int
-        |  def indexOf(x$1: Int, x$2: Int): Int
-        |  def lastIndexOf(x$1: Int): Int
-        |  def lastIndexOf(x$1: Int, x$2: Int): Int
-        |  def indexOf(x$1: String): Int
-        |  def indexOf(x$1: String, x$2: Int): Int
-        |  def lastIndexOf(x$1: String): Int
-        |  def lastIndexOf(x$1: String, x$2: Int): Int
-        |  def substring(x$1: Int): String
-        |  def substring(x$1: Int, x$2: Int): String
-        |  def subSequence(x$1: Int, x$2: Int): CharSequence
-        |  def concat(x$1: String): String
-        |  def replace(x$1: Char, x$2: Char): String
-        |  def matches(x$1: String): Boolean
-        |  def contains(x$1: CharSequence): Boolean
-        |  def replaceFirst(x$1: String, x$2: String): String
-        |  def replaceAll(x$1: String, x$2: String): String
-        |  def replace(x$1: CharSequence, x$2: CharSequence): String
-        |  def split(x$1: String, x$2: Int): Array[String]
-        |  def split(x$1: String): Array[String]
-        |  def toLowerCase(x$1: java.util.Locale): String
-        |  def toLowerCase(): String
-        |  def toUpperCase(x$1: java.util.Locale): String
-        |  def toUpperCase(): String
-        |  def trim(): String
-        |  def toCharArray(): Array[Char]
-        |  def intern(): String
-        |  final def +(x$1: Any): String
-        |  def chars(): java.util.stream.IntStream
-        |  def codePoints(): java.util.stream.IntStream
+  test("Date API") {
+    assert(ApiInfo.materialize[JDate].repr ==
+      """java.util.Date {
+        |  def getYear(): Int
+        |  def setYear(x$1: Int): Unit
+        |  def getMonth(): Int
+        |  def setMonth(x$1: Int): Unit
+        |  def getDate(): Int
+        |  def setDate(x$1: Int): Unit
+        |  def getDay(): Int
+        |  def getHours(): Int
+        |  def setHours(x$1: Int): Unit
+        |  def getMinutes(): Int
+        |  def setMinutes(x$1: Int): Unit
+        |  def getSeconds(): Int
+        |  def setSeconds(x$1: Int): Unit
+        |  def getTime(): Long
+        |  def setTime(x$1: Long): Unit
+        |  def before(x$1: java.util.Date): Boolean
+        |  def after(x$1: java.util.Date): Boolean
+        |  def compareTo(x$1: java.util.Date): Int
+        |  def toLocaleString(): String
+        |  def toGMTString(): String
+        |  def getTimezoneOffset(): Int
+        |  def toInstant(): java.time.Instant
         |}""".stripMargin
     )
   }
