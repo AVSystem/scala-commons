855d4fad77fa5c6bc5c5fd52d49012b07d4c2a24
ghik <romeqjanoosh@gmail.com>
2020-04-25 10:39:35 +0200

    added UndefOr<->Opt converters in JsInterop

--- commons-core/js/src/main/scala/com/avsystem/commons/jsiop/JsInterop.scala
+++ commons-core/js/src/main/scala/com/avsystem/commons/jsiop/JsInterop.scala
@@ -1,12 +1,25 @@
 package com.avsystem.commons
 package jsiop
 
+import com.avsystem.commons.jsiop.JsInterop.{JsOptOps, UndefOrOps}
 import com.avsystem.commons.misc.TimestampConversions
 
 import scala.scalajs.js
+import scala.scalajs.js.UndefOr
 
 trait JsInterop {
   implicit def jsDateTimestampConversions(jsDate: js.Date): TimestampConversions =
     new TimestampConversions(jsDate.getTime.toLong)
+
+  implicit def undefOrOps[A](undefOr: UndefOr[A]): UndefOrOps[A] = new UndefOrOps(undefOr)
+  implicit def jsOptOps[A](opt: Opt[A]): JsOptOps[A] = new JsOptOps(opt)
 }
-object JsInterop extends JsInterop
+object JsInterop extends JsInterop {
+  class UndefOrOps[A](private val value: UndefOr[A]) extends AnyVal {
+    def toOpt: Opt[A] = if (value.isDefined) Opt(value.get) else Opt.Empty
+  }
+
+  class JsOptOps[A](private val value: Opt[A]) extends AnyVal {
+    def orUndefined: UndefOr[A] = if (value.isDefined) js.defined(value.get) else js.undefined
+  }
+}
