bc7a28d14a55c0f783d863a512087330f6c0f313
ghik <romeqjanoosh@gmail.com>
2020-05-14 21:51:02 +0200

    imports fixed

--- commons-redis/src/main/scala/com/avsystem/commons/redis/commands/cluster.scala
+++ commons-redis/src/main/scala/com/avsystem/commons/redis/commands/cluster.scala
@@ -1,7 +1,7 @@
 package com.avsystem.commons
 package redis.commands
 
-import com.avsystem.commons.misc._
+import com.avsystem.commons.misc.{Opt => _, OptArg => _, _}
 import com.avsystem.commons.redis.CommandEncoder.CommandArg
 import com.avsystem.commons.redis._
 import com.avsystem.commons.redis.commands.ReplyDecoders._
