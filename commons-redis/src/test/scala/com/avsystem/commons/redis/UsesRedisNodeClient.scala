ce820e738395ebc9fee2516db5eb2d8c0dd6d446
ghik <romeqjanoosh@gmail.com>
2020-05-14 16:47:07 +0200

    some more tests for TLS

--- commons-redis/src/test/scala/com/avsystem/commons/redis/UsesRedisNodeClient.scala
+++ commons-redis/src/test/scala/com/avsystem/commons/redis/UsesRedisNodeClient.scala
@@ -1,21 +1,29 @@
 package com.avsystem.commons
 package redis
 
-import com.avsystem.commons.redis.config.NodeConfig
+import com.avsystem.commons.redis.config.{ConnectionConfig, NodeConfig, TlsConfig}
 import org.scalatest.Suite
 
 /**
   * Author: ghik
   * Created: 14/04/16.
   */
-trait UsesRedisNodeClient extends UsesRedisServer with UsesActorSystem { this: Suite =>
-  def nodeConfig: NodeConfig = NodeConfig()
+trait UsesRedisNodeClient extends UsesRedisServer with UsesActorSystem with UsesSslContext { this: Suite =>
+  def useTls: Boolean = false
+
+  def connectionConfig: ConnectionConfig =
+    ConnectionConfig(tlsConfig = if (useTls) OptArg(TlsConfig(sslContext)) else OptArg.Empty)
+
+  def nodeConfig: NodeConfig = NodeConfig(
+    connectionConfigs = _ => connectionConfig,
+    blockingConnectionConfigs = _ => connectionConfig
+  )
 
   var redisClient: RedisNodeClient = _
 
   override protected def beforeAll(): Unit = {
     super.beforeAll()
-    redisClient = new RedisNodeClient(address, nodeConfig)
+    redisClient = new RedisNodeClient(if (useTls) tlsAddress else address, nodeConfig)
   }
 
   override protected def afterAll(): Unit = {
