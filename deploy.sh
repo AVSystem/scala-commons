876e3895f939a3b28d9ded0586beb7f09c4f4330
ghik <romeqjanoosh@gmail.com>
2020-08-18 13:49:51 +0200

    added scala.js 0.6 cross build

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 openssl aes-256-cbc -K $encrypted_b21e52cd6f46_key -iv $encrypted_b21e52cd6f46_iv -in travis/secrets.tar.enc -out travis/local.secrets.tar -d
 tar xv -C travis -f travis/local.secrets.tar
 sbt +publishSigned
-SCALAJS_VERSION=1.0.1 sbt +commons-js/publishSigned
+SCALAJS_VERSION=0.6.33 sbt +commons-js/publishSigned
 sbt sonatypeBundleRelease
