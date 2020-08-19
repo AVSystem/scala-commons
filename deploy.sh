62c2e04bab1f6e5c94c0ffb83d31ad3aae048615
ghik <romeqjanoosh@gmail.com>
2020-08-19 10:37:25 +0200

    sbt color on CI

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 openssl aes-256-cbc -K $encrypted_b21e52cd6f46_key -iv $encrypted_b21e52cd6f46_iv -in travis/secrets.tar.enc -out travis/local.secrets.tar -d
 tar xv -C travis -f travis/local.secrets.tar
-sbt +publishSigned
-SCALAJS_VERSION=1.0.1 sbt +commons-js/publishSigned
-sbt sonatypeBundleRelease
+sbt -Dsbt.color=true +publishSigned
+SCALAJS_VERSION=1.0.1 sbt -Dsbt.color=true +commons-js/publishSigned
+sbt -Dsbt.color=true sonatypeBundleRelease
