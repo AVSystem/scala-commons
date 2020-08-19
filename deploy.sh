b458f480047111b18fc6870729c34986d9a6ee29
ghik <romeqjanoosh@gmail.com>
2020-08-19 10:58:47 +0200

    update scalajs1 to 1.1.1

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 openssl aes-256-cbc -K $encrypted_b21e52cd6f46_key -iv $encrypted_b21e52cd6f46_iv -in travis/secrets.tar.enc -out travis/local.secrets.tar -d
 tar xv -C travis -f travis/local.secrets.tar
 sbt -Dsbt.color=true +publishSigned
-SCALAJS_VERSION=1.0.1 sbt -Dsbt.color=true +commons-js/publishSigned
+SCALAJS_VERSION=1.1.1 sbt -Dsbt.color=true +commons-js/publishSigned
 sbt -Dsbt.color=true sonatypeBundleRelease
