6c3c3f5cc1cc40de04e4a2718228db9592cfac14
Sebastian Haracz <sebastian.haracz@gmail.com>
2020-09-04 14:06:26 +0200

    Bump mongo driver to 4.1.0 and remove deprecated usages

--- commons-mongo/src/main/scala/com/avsystem/commons/mongo/async/GenCodecCollection.scala
+++ commons-mongo/src/main/scala/com/avsystem/commons/mongo/async/GenCodecCollection.scala
@@ -3,14 +3,15 @@ package mongo.async
 
 import com.avsystem.commons.mongo.core.GenCodecRegistry
 import com.avsystem.commons.serialization.GenCodec
-import com.github.ghik.silencer.silent
-import com.mongodb.async.client.{MongoCollection, MongoDatabase}
+import org.mongodb.scala.{MongoCollection, MongoDatabase}
 
 object GenCodecCollection {
-  @silent("deprecated")
-  def create[T: GenCodec : ClassTag](db: MongoDatabase, name: String,
-    legacyOptionEncoding: Boolean = GenCodecRegistry.LegacyOptionEncoding): MongoCollection[T] = {
-    val newRegistry = GenCodecRegistry.create[T](db.getCodecRegistry, legacyOptionEncoding)
-    db.withCodecRegistry(newRegistry).getCollection(name, classTag[T].runtimeClass.asInstanceOf[Class[T]])
+  def create[T: GenCodec : ClassTag](
+    db: MongoDatabase,
+    name: String,
+    legacyOptionEncoding: Boolean = GenCodecRegistry.LegacyOptionEncoding,
+  ): MongoCollection[T] = {
+    val newRegistry = GenCodecRegistry.create[T](db.codecRegistry, legacyOptionEncoding)
+    db.withCodecRegistry(newRegistry).getCollection(name)
   }
 }
