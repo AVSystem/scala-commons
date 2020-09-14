d4f14c780d935b642082cf432ff494c473045332
ghik <romeqjanoosh@gmail.com>
2020-09-14 15:02:22 +0200

    analyzer plugin can now check JDK version required for compilation

--- commons-analyzer/src/main/scala/com/avsystem/commons/analyzer/AnalyzerPlugin.scala
+++ commons-analyzer/src/main/scala/com/avsystem/commons/analyzer/AnalyzerPlugin.scala
@@ -1,6 +1,7 @@
 package com.avsystem.commons
 package analyzer
 
+import scala.reflect.internal.util.NoPosition
 import scala.tools.nsc.plugins.{Plugin, PluginComponent}
 import scala.tools.nsc.{Global, Phase}
 
@@ -8,26 +9,35 @@ final class AnalyzerPlugin(val global: Global) extends Plugin { plugin =>
 
   override def init(options: List[String], error: String => Unit): Boolean = {
     options.foreach { option =>
-      val level = option.charAt(0) match {
-        case '-' => Level.Off
-        case '*' => Level.Info
-        case '+' => Level.Error
-        case _ => Level.Warn
-      }
-      val nameArg = if (level != Level.Warn) option.drop(1) else option
-      if (nameArg == "_") {
-        rules.foreach(_.level = level)
-      } else {
-        val (name, arg) = nameArg.split(":", 2) match {
-          case Array(n, a) => (n, a)
-          case Array(n) => (n, null)
+      if (option.startsWith("requireJDK=")) {
+        val jdkVersionRegex = option.substring(option.indexOf('=') + 1)
+        val javaVersion = System.getProperty("java.version", "")
+        if (!javaVersion.matches(jdkVersionRegex)) {
+          global.reporter.error(NoPosition,
+            s"This project must be compiled on JDK version that matches $jdkVersionRegex but got $javaVersion")
         }
-        rulesByName.get(name) match {
-          case Some(rule) =>
-            rule.level = level
-            rule.argument = arg
-          case None =>
-            error(s"Unrecognized AVS analyzer rule: $name")
+      } else {
+        val level = option.charAt(0) match {
+          case '-' => Level.Off
+          case '*' => Level.Info
+          case '+' => Level.Error
+          case _ => Level.Warn
+        }
+        val nameArg = if (level != Level.Warn) option.drop(1) else option
+        if (nameArg == "_") {
+          rules.foreach(_.level = level)
+        } else {
+          val (name, arg) = nameArg.split(":", 2) match {
+            case Array(n, a) => (n, a)
+            case Array(n) => (n, null)
+          }
+          rulesByName.get(name) match {
+            case Some(rule) =>
+              rule.level = level
+              rule.argument = arg
+            case None =>
+              error(s"Unrecognized AVS analyzer rule: $name")
+          }
         }
       }
     }
