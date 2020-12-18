7a92a76e433e3ceae4e943e7de55caa32aaf640b
ghik <romeqjanoosh@gmail.com>
2020-12-18 23:56:09 +0100

    github actions: troubleshooting

--- install-redis.sh
+++ install-redis.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 REDIS_VERSION=$1
+echo $0 $1 $2 $REDIS_VERSION
 if [ ! -d ./redis-$REDIS_VERSION/src ]; then
     wget http://download.redis.io/releases/redis-$REDIS_VERSION.tar.gz
     tar -xzf redis-$REDIS_VERSION.tar.gz
