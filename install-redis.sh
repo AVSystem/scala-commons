6285cc0e65c5a7624d3812e919524f784ade1f4a
ghik <romeqjanoosh@gmail.com>
2020-12-19 00:33:57 +0100

    github actions: troubleshooting

--- install-redis.sh
+++ install-redis.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-if [ ! -d ./redis-$REDIS_VERSION ]; then
+REDIS_HOME=$PWD/redis-$REDIS_VERSION/src
+echo "REDIS_HOME=$REDIS_HOME" >> $GITHUB_ENV
+if [ ! -d $REDIS_HOME ]; then
     wget http://download.redis.io/releases/redis-$REDIS_VERSION.tar.gz
     tar -xzf redis-$REDIS_VERSION.tar.gz
     cd redis-$REDIS_VERSION && make BUILD_TLS=yes
 fi
-echo "REDIS_HOME=./redis-$REDIS_VERSION/src" >> $GITHUB_ENV
+
