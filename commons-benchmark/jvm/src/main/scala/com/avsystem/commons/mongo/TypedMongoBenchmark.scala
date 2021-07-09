a6a44de01465e752d2da550a6cb7dffe66fc18aa
ghik <romeqjanoosh@gmail.com>
2021-07-09 13:46:27 +0200

    added simple mongo client benchmark

---
+++ commons-benchmark/jvm/src/main/scala/com/avsystem/commons/mongo/TypedMongoBenchmark.scala
@@ -0,0 +1,63 @@
+package com.avsystem.commons
+package mongo
+
+import com.avsystem.commons.mongo.TypedMongoBenchmark.BatchSize
+import com.avsystem.commons.mongo.typed.{MongoEntity, MongoEntityCompanion, TypedMongoCollection}
+import com.mongodb.{ConnectionString, MongoClientSettings}
+import com.mongodb.connection.{AsynchronousSocketChannelStreamFactoryFactory, ConnectionPoolSettings}
+import com.mongodb.reactivestreams.client.{MongoClient, MongoClients}
+import monix.eval.Task
+import monix.execution.Scheduler
+import org.openjdk.jmh.annotations._
+
+import java.nio.channels.AsynchronousChannelGroup
+import java.util.concurrent.{ExecutorService, Executors, ForkJoinPool}
+
+case class BenchEntity(
+  id: String,
+  str: String,
+  int: Int,
+  list: List[String]
+) extends MongoEntity[String]
+object BenchEntity extends MongoEntityCompanion[BenchEntity]
+
+object TypedMongoBenchmark {
+  final val BatchSize = 1024
+}
+
+@Warmup(iterations = 2)
+@Measurement(iterations = 5)
+@Fork(1)
+@BenchmarkMode(Array(Mode.Throughput))
+@State(Scope.Thread)
+class TypedMongoBenchmark {
+  val threadPool: ExecutorService = Executors.newWorkStealingPool()
+  implicit val scheduler: Scheduler = Scheduler(threadPool)
+
+  val channelGroup: AsynchronousChannelGroup = AsynchronousChannelGroup.withThreadPool(threadPool)
+  val mongoClientSettings: MongoClientSettings =
+    MongoClientSettings.builder
+      .applyConnectionString(new ConnectionString("mongodb://ghikomp/test"))
+      .streamFactoryFactory(AsynchronousSocketChannelStreamFactoryFactory.builder.group(channelGroup).build)
+      .build
+
+  val mongoClient: MongoClient = MongoClients.create(mongoClientSettings)
+  val coll = new TypedMongoCollection[BenchEntity](mongoClient.getDatabase("bench").getCollection("BenchEntity"))
+
+  val task: Task[List[Option[BenchEntity]]] =
+    Task.parSequence(List.fill(BatchSize)(coll.findById("foo")))
+
+  @Benchmark
+  @OperationsPerInvocation(BatchSize)
+  def benc: List[Option[BenchEntity]] =
+    task.runSyncUnsafe()
+}
+
+object TypedMongoBenchmarkManual {
+  def main(args: Array[String]): Unit = {
+    val tmb = new TypedMongoBenchmark
+    while (true) {
+      tmb.benc
+    }
+  }
+}
