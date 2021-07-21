e96b9cf1abc3bbd2abd958b222ec919b1fdd90eb
ghik <romeqjanoosh@gmail.com>
2021-07-21 10:37:21 +0200

    mention null handling in optionalParam scaladoc

--- commons-core/src/main/scala/com/avsystem/commons/serialization/optionalParam.scala
+++ commons-core/src/main/scala/com/avsystem/commons/serialization/optionalParam.scala
@@ -7,8 +7,13 @@ package serialization
   *
   * Makes the parameter optional, meaning that its type must be an `Option`, `Opt`, `OptArg` or any other type
   * that has an instance of [[com.avsystem.commons.meta.OptionLike OptionLike]].
-  * When the value is empty, the resulting JSON object or map of raw values simply lacks the entry
-  * corresponding to this parameter.
+  * When writing, the resulting object (e.g. JSON) or map of raw values simply lacks the entry corresponding
+  * to this parameter if its value is empty (as defined by `OptionLike` instance).
+  *
+  * When reading, a complete lack of a field in an object (e.g. JSON) is interpreted as an empty value.
+  * Depending on the exact param type (e.g. `Opt` vs `NOpt`), `null` value may also be interpreted as a complete
+  * lack of a value. This is controlled by `OptionLike.ignoreNulls` which is `true` for all option-like
+  * types except `NOpt`.
   *
   * [[optionalParam]] is an alternative, better way of expressing optional params as opposed to usage of
   * [[whenAbsent]] with [[transientDefault]], i.e.
