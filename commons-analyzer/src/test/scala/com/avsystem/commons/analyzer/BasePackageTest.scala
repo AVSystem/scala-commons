c32b19f66c1bfa52edda779456e96a03d2cfc54f
Roman Janusz <r.janusz@avsystem.com>
2023-03-09 17:56:04 +0100

    analyzer rule to check for a required base package

---
+++ commons-analyzer/src/test/scala/com/avsystem/commons/analyzer/BasePackageTest.scala
@@ -0,0 +1,62 @@
+package com.avsystem.commons
+package analyzer
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class BasePackageTest extends AnyFunSuite with AnalyzerTest {
+  settings.pluginOptions.value ++= List("AVSystemAnalyzer:+basePackage:com.avsystem.commons")
+
+  test("base package only") {
+    assertNoErrors(
+      """
+        |package com.avsystem.commons
+        |
+        |object bar
+        |""".stripMargin)
+  }
+
+  test("chained base package") {
+    assertNoErrors(
+      """
+        |package com.avsystem
+        |package commons
+        |
+        |object bar
+        |""".stripMargin)
+  }
+
+  test("base package with chained subpackage") {
+    assertNoErrors(
+      """
+        |package com.avsystem.commons
+        |package core
+        |
+        |object bar
+        |""".stripMargin)
+  }
+
+  test("no base package") {
+    assertErrors(1,
+      """
+        |object bar
+        |""".stripMargin)
+  }
+
+  test("wrong base package") {
+    assertErrors(1,
+      """
+        |package com.avsystem.kommons
+        |
+        |object bar
+        |""".stripMargin)
+  }
+
+  test("unchained subpackage") {
+    assertErrors(1,
+      """
+        |package com.avsystem.commons.core
+        |
+        |object bar
+        |""".stripMargin)
+  }
+}
