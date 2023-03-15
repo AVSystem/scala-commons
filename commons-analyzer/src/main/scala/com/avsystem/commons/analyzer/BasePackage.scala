1c2d0aa1270a6bd706d28488362c01638f50bad0
Sebastian Haracz <sebastian.haracz@gmail.com>
2023-03-15 23:03:47 +0100

    Handle imports when checking required base package - rename

--- commons-analyzer/src/main/scala/com/avsystem/commons/analyzer/BasePackage.scala
+++ commons-analyzer/src/main/scala/com/avsystem/commons/analyzer/BasePackage.scala
@@ -8,17 +8,15 @@ class BasePackage(g: Global) extends AnalyzerRule(g, "basePackage") {
 
   import global._
 
-  object ImportsList {
-    @tailrec def unapply(stats: List[Tree]): Option[Tree] =
-      stats match {
-        case Nil => None
-        case List(stat) => Some(stat)
-        case head :: next =>
-          head match {
-            case Import(_, _) => unapply(next)
-            case _ => None
-          }
+  object ImportsOnlyOrEmptyList {
+    @tailrec def unapply(stats: List[Tree]): Option[Tree] = stats match {
+      case Nil => None
+      case List(stat) => Some(stat)
+      case head :: next => head match {
+        case Import(_, _) => unapply(next)
+        case _ => None
       }
+    }
   }
 
   def analyze(unit: CompilationUnit): Unit = if (argument != null) {
@@ -26,7 +24,7 @@ class BasePackage(g: Global) extends AnalyzerRule(g, "basePackage") {
 
     @tailrec def validate(tree: Tree): Unit = tree match {
       case PackageDef(pid, _) if pid.symbol.hasPackageFlag && pid.symbol.fullName == requiredBasePackage =>
-      case PackageDef(_, ImportsList(stat)) => validate(stat)
+      case PackageDef(_, ImportsOnlyOrEmptyList(stat)) => validate(stat)
       case t => report(t.pos, s"`$requiredBasePackage` must be one of the base packages in this file")
     }
 
