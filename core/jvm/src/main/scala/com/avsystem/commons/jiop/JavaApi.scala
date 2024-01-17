eb7fb9e998ae1254c8b9a8d0df4bd5b26a500abe
Sebastian Haracz <sebastian.haracz@gmail.com>
2024-01-17 14:17:59 +0100

    Add JavaApi annotation - doc fix

--- core/jvm/src/main/scala/com/avsystem/commons/jiop/JavaApi.scala
+++ core/jvm/src/main/scala/com/avsystem/commons/jiop/JavaApi.scala
@@ -2,6 +2,6 @@ package com.avsystem.commons
 package jiop
 
 /**
-  * Marks Scala methods which exists only for Java compatibility / interoperability
+  * Marks Scala method which exists only for Java compatibility / interoperability
   */
 class JavaApi extends StaticAnnotation
