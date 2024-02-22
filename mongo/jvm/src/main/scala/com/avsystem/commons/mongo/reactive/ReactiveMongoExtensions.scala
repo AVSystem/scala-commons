c493ed7b1d2f87b0d0810bc0bbe4e4506f045b69
Sebastian Haracz <sebastian.haracz@gmail.com>
2024-02-22 15:52:30 +0100

    Reactive Publisher to Monix extensions for typed Mongo - comments

--- mongo/jvm/src/main/scala/com/avsystem/commons/mongo/reactive/ReactiveMongoExtensions.scala
+++ mongo/jvm/src/main/scala/com/avsystem/commons/mongo/reactive/ReactiveMongoExtensions.scala
@@ -21,9 +21,11 @@ object ReactiveMongoExtensions extends ReactiveMongoExtensions {
     // mongo observable implementation sometimes logs an error on server-closed cursors
     private def singleObservable: Task[Option[T]] = Task.fromReactivePublisher(publisher)
 
-    def headOptL: Task[Opt[T]] = singleObservable.map(_.toOpt)
+    // handles both an empty Publisher and and a single null item
     def headOptionL: Task[Option[T]] = singleObservable.map(_.filterNot(_ == null))
+    def headOptL: Task[Opt[T]] = headOptionL.map(_.toOpt)
+    // can return null if Mongo driver for some reason returns null, intentionally don't want to report failure
     def headL: Task[T] = singleObservable.map(_.get)
-    def completedL: Task[Unit] = singleObservable.void
+    def completedL: Task[Unit] = headOptionL.void
   }
 }
