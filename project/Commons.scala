60e6f1105f0d4847c4844fe7ebeb57d0462e35e1
Scala Steward <scala_steward@virtuslab.com>
2024-09-12 13:39:58 +0000

    Update spring-context to 6.1.13

--- project/Commons.scala
+++ project/Commons.scala
@@ -31,7 +31,7 @@ object Commons extends ProjectGroup("commons") {
   val scalacheckVersion = "1.18.0"
   val jettyVersion = "12.0.13"
   val mongoVersion = "5.1.4"
-  val springVersion = "5.3.39"
+  val springVersion = "6.1.13"
   val typesafeConfigVersion = "1.4.3"
   val commonsIoVersion = "1.3.2" // test only
   val scalaLoggingVersion = "3.9.5"
