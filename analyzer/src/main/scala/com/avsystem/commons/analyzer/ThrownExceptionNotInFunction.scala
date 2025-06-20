4a7b996fce5c59dedf1ba01b9d10e9842fa6a3e5
Bartłomiej Kozak <bartlomiejkozak@proton.me>
2025-06-20 21:27:52 +0200

    better error message

--- analyzer/src/main/scala/com/avsystem/commons/analyzer/ThrownExceptionNotInFunction.scala
+++ analyzer/src/main/scala/com/avsystem/commons/analyzer/ThrownExceptionNotInFunction.scala
@@ -11,7 +11,7 @@ final class ThrownExceptionNotInFunction(g: Global) extends AnalyzerRule(g, "thr
     case Apply(f: Tree, args: List[Tree]) =>
       args.zip(f.tpe.params).foreach {
         case (arg, param) if definitions.isFunctionType(param.tpe) && arg.tpe <:< definitions.NothingTpe =>
-          report(arg.pos, "exception thrown in place of function definition")
+          report(arg.pos, "Exception thrown in place of function definition. Wrap the exception in a function literal instead (e.g. `_ => throw ex` instead of `throw ex`)")
         case (_, _) =>
       }
   })
