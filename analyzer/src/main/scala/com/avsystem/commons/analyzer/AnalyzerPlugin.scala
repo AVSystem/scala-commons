c01d891664e256ecf278ccaed2c9f07b53cf006e
Bartłomiej Kozak <b.kozak@avsystem.com>
2025-06-26 10:21:15 +0200

    add NothingAsFunctionArgument rule to the Analyzer Plugin (#585)
    * add ThrownExceptionNotInMonixScope rule to the Analyzer Plugin
    
    * change to all function parameters
    
    * add some `flatMap` cases
    
    * Nothing is a special bottom type; it is a subtype of every other type. The Scala compiler loves to infer Nothing as a generic type but that is almost always incorrect. Explicit type arguments should be used instead.
    
    * Revert "Nothing is a special bottom type; it is a subtype of every other type. The Scala compiler loves to infer Nothing as a generic type but that is almost always incorrect. Explicit type arguments should be used instead."
    
    This reverts commit 9622a3f735902170f64530e0ed9cba5f8399cb99.
    
    * handle multiple arguments, not only the first one
    
    * add more tests
    
    * add more tests
    
    * add and fix test
    
    * lil enhancement
    
    * better error message
    
    * Refactor `ThrownExceptionNotInFunctionTest` using `AnyWordSpec` and `ScalaInterpolator` for improved readability and maintainability.
    
    * Enhance `ThrownExceptionNotInFunctionTest` for clarity and accuracy in exception detection scenarios
    
    * renaming + docs
    
    * Rename `ThrowAsFunctionArgument` to `NothingAsFunctionArgument`, update related documentation and tests

--- analyzer/src/main/scala/com/avsystem/commons/analyzer/AnalyzerPlugin.scala
+++ analyzer/src/main/scala/com/avsystem/commons/analyzer/AnalyzerPlugin.scala
@@ -56,6 +56,7 @@ final class AnalyzerPlugin(val global: Global) extends Plugin { plugin =>
     new Any2StringAdd(global),
     new ThrowableObjects(global),
     new DiscardedMonixTask(global),
+    new NothingAsFunctionArgument(global),
     new BadSingletonComponent(global),
     new ConstantDeclarations(global),
     new BasePackage(global),
