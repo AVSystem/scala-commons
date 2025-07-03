f28048b57a946a1d9df2fc06035735762ec0a1ec
Scala Steward <scala_steward@virtuslab.com>
2025-07-03 14:48:53 +0000

    Update jetty-client to 12.0.23

--- project/Commons.scala
+++ project/Commons.scala
@@ -28,7 +28,7 @@ object Commons extends ProjectGroup("commons") {
   val scalatestVersion = "3.2.19"
   val scalatestplusScalacheckVersion = "3.2.14.0"
   val scalacheckVersion = "1.18.1"
-  val jettyVersion = "12.0.22"
+  val jettyVersion = "12.0.23"
   val mongoVersion = "5.5.1"
   val springVersion = "5.3.39"
   val typesafeConfigVersion = "1.4.3"
