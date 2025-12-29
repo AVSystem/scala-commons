5a3b1a4938fe1896d049f0c998941e64ad2838f1
Dawid Dworak <dawid.dworak@gmail.com>
2025-12-29 14:35:24 +0100

    Remove circe and upickle benchmarks
    Currently, those are mostly maintenance burden to update. I'm keeping the benchmarks for testing our own code as needed.

--- benchmark/js/src/main/scala/com/avsystem/commons/ser/JsonBenchmarks.scala
+++ benchmark/js/src/main/scala/com/avsystem/commons/ser/JsonBenchmarks.scala
@@ -3,8 +3,6 @@ package ser
 
 import com.avsystem.commons.serialization.json.{JsonStringInput, JsonStringOutput}
 import com.avsystem.commons.serialization.nativejs.{NativeJsonInput, NativeJsonOutput}
-import io.circe.parser._
-import io.circe.syntax._
 import japgolly.scalajs.benchmark.gui.GuiSuite
 import japgolly.scalajs.benchmark.{Benchmark, Suite}
 
@@ -17,23 +15,11 @@ object JsonBenchmarks {
       Benchmark("Writing case class: GenCodec, Native Json format") {
         NativeJsonOutput.writeAsString(Something.Example)
       },
-      Benchmark("Writing case class: Circe") {
-        Something.Example.asJson.noSpaces
-      },
-      Benchmark("Writing case class: uPickle") {
-        upickle.default.write(Something.Example)
-      },
       Benchmark("Reading case class: GenCodec, String Json format") {
         JsonStringInput.read[Something](Something.ExampleJsonString)
       },
       Benchmark("Reading case class: GenCodec, Native Json format") {
         NativeJsonInput.readString[Something](Something.ExampleJsonString)
-      },
-      Benchmark("Reading case class: Circe") {
-        decode[Something](Something.ExampleJsonString).fold(e => throw e, identity)
-      },
-      Benchmark("Reading case class: uPickle") {
-        upickle.default.read[Something](Something.ExampleJsonString)
       },
 
       Benchmark("Writing sealed hierarchy: GenCodec, String Json format") {
@@ -48,12 +34,6 @@ object JsonBenchmarks {
       Benchmark("Writing sealed hierarchy: GenCodec (flat), Native Json format") {
         NativeJsonOutput.writeAsString(FlatSealedStuff.ExampleList)
       },
-      Benchmark("Writing sealed hierarchy: Circe") {
-        SealedStuff.ExampleList.asJson.noSpaces
-      },
-      Benchmark("Writing sealed hierarchy: uPickle") {
-        upickle.default.write(SealedStuff.ExampleList)
-      },
       Benchmark("Reading sealed hierarchy: GenCodec, String Json format") {
         JsonStringInput.read[List[SealedStuff]](SealedStuff.ExampleJsonString)
       },
@@ -66,12 +46,6 @@ object JsonBenchmarks {
       Benchmark("Reading sealed hierarchy: GenCodec (flat), Native Json format") {
         NativeJsonInput.readString[List[FlatSealedStuff]](FlatSealedStuff.ExampleJsonString)
       },
-      Benchmark("Reading sealed hierarchy: Circe") {
-        decode[List[SealedStuff]](SealedStuff.ExampleJsonString).fold(e => throw e, identity)
-      },
-      Benchmark("Reading sealed hierarchy: uPickle") {
-        upickle.default.read[List[SealedStuff]](SealedStuff.ExampleUpickleJsonString)
-      },
 
       Benchmark("Writing foos: GenCodec, String Json format") {
         JsonStringOutput.write(Foo.ExampleMap)
@@ -79,24 +53,12 @@ object JsonBenchmarks {
       Benchmark("Writing foos: GenCodec, Native Json format") {
         NativeJsonOutput.writeAsString(Foo.ExampleMap)
       },
-      Benchmark("Writing foos: Circe") {
-        Foo.ExampleMap.asJson.noSpaces
-      },
-      Benchmark("Writing foos: uPickle") {
-        upickle.default.write(Foo.ExampleMap)
-      },
       Benchmark("Reading foos: GenCodec, String Json format") {
         JsonStringInput.read[Map[String, Foo]](Foo.ExampleJsonString)
       },
       Benchmark("Reading foos: GenCodec with Native Json format") {
         NativeJsonInput.readString[Map[String, Foo]](Foo.ExampleJsonString)
       },
-      Benchmark("Reading foos: Circe") {
-        decode[Map[String, Foo]](Foo.ExampleJsonString).fold(e => throw e, identity)
-      },
-      Benchmark("Reading foos: uPickle") {
-        upickle.default.read[Map[String, Foo]](Foo.ExampleUpickleJsonString)
-      }
     )
   )
 }
