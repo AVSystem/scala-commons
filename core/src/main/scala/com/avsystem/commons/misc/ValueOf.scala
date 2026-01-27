e0f03bc4a57089b50ea482ee66b7f6cf144a199e
Bartłomiej Kozak <bartlomiejkozak@proton.me>
2026-01-27 10:23:03 +0100

    Remove `ValueOf` macro implementation and related annotations

--- core/src/main/scala/com/avsystem/commons/misc/ValueOf.scala
+++ core/src/main/scala/com/avsystem/commons/misc/ValueOf.scala
@@ -1,15 +1,6 @@
 package com.avsystem.commons
 package misc
 
-import scala.annotation.implicitNotFound
-
-/**
- * Macro materialized typeclass which captures the single value of a singleton type.
- */
-@implicitNotFound("Cannot derive value of ${T} - is not a singleton type")
-class ValueOf[T](val value: T) extends AnyVal
 object ValueOf {
-  inline given [T] => ValueOf[T] = ${ mkValueOfImpl[T] }
   def apply[T](using vof: ValueOf[T]): T = vof.value
-  def mkValueOfImpl[T: Type](using Quotes): Expr[ValueOf[T]] = '{ ??? }.asInstanceOf[Expr[ValueOf[T]]]
 }
