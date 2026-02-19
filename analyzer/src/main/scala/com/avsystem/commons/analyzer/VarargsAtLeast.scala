9eccf0de31558fcb7fdf66d653e39e1c079fbf02
Bartłomiej Kozak <bartlomiejkozak@proton.me>
2026-02-19 09:36:23 +0100

    feat(03-05): implement VarargsAtLeast rule with Scala 3 SeqLiteral vararg detection
    - Rewrite from Scala 2 to Scala 3 API using transformApply and paramInfoss
    - Detect varargs via isRepeatedParam on parameter type info
    - Distinguish compiler-generated SeqLiteral from explicit splice (seq*)
    - Count SeqLiteral elements to validate @atLeast annotation
    - Extract annotation class via Symbols.getClassIfDefined
    - Add tests for too-few args, sufficient args, and collection splice

--- analyzer/src/main/scala/com/avsystem/commons/analyzer/VarargsAtLeast.scala
+++ analyzer/src/main/scala/com/avsystem/commons/analyzer/VarargsAtLeast.scala
@@ -1,40 +1,73 @@
 package com.avsystem.commons
 package analyzer
 
-import scala.tools.nsc.Global
+import dotty.tools.dotc.ast.tpd
+import dotty.tools.dotc.core.Contexts.Context
+import dotty.tools.dotc.core.Symbols
+import dotty.tools.dotc.core.Symbols.NoSymbol
+import dotty.tools.dotc.core.Constants.Constant
 
-class VarargsAtLeast(g: Global) extends AnalyzerRule(g, "varargsAtLeast") {
+class VarargsAtLeast extends AnalyzerRule {
+  val name: String = "varargsAtLeast"
 
-  import global._
+  override def transformApply(tree: tpd.Apply)(using ctx: Context): tpd.Tree = {
+    val fun = tree.fun
+    val args = tree.args
 
-  lazy val atLeastAnnotTpe: Type = classType("com.avsystem.commons.annotation.atLeast")
-
-  def analyze(unit: CompilationUnit): Unit = if (atLeastAnnotTpe != NoType) {
-    def isVarargParam(tree: Tree) = tree match {
-      case Typed(_, Ident(typeNames.WILDCARD_STAR)) => true
-      case _ => false
-    }
-
-    unit.body.foreach(analyzeTree {
-      case t @ Apply(fun, args)
-          if fun.tpe != null &&
-            fun.tpe.params.lastOption.map(_.tpe.typeSymbol).contains(definitions.RepeatedParamClass) &&
-            !args.lastOption.exists(isVarargParam) =>
-
-        val required =
-          fun.tpe.params.last.annotations
-            .find(_.tree.tpe <:< atLeastAnnotTpe)
-            .map(_.tree.children.tail)
-            .collect { case List(Literal(Constant(n: Int))) =>
-              n
-            }
-            .getOrElse(0)
-
-        val actual = args.size - fun.tpe.params.size + 1
-
-        if (actual < required) {
-          report(t.pos, s"This method requires at least $required arguments for its repeated parameter, $actual passed.")
+    if (fun.symbol != NoSymbol) {
+      val paramInfoss = fun.symbol.info.paramInfoss
+      if (paramInfoss.nonEmpty) {
+        val params = paramInfoss.head
+        if (params.nonEmpty && params.last.isRepeatedParam) {
+          // In Scala 3, varargs are always passed as a single Typed(SeqLiteral(...), tpt) node.
+          // Explicit splices (seq*) are Typed(expr, tpt) where expr is NOT a SeqLiteral.
+          // We need to look inside the Typed to distinguish and count elements.
+          args.lastOption match {
+            case Some(tpd.Typed(seqLit: tpd.SeqLiteral, _)) =>
+              // Compiler-generated vararg pack -- count the elements
+              checkAtLeast(tree, fun, params, seqLit.elems.size)
+            case Some(_: tpd.Typed) =>
+              // Explicit splice (e.g., list*) -- skip the check
+              ()
+            case _ =>
+              // No Typed wrapper (shouldn't happen for varargs, but handle gracefully)
+              ()
+          }
         }
-    })
+      }
+    }
+    tree
+  }
+
+  private def checkAtLeast(
+    tree: tpd.Apply,
+    fun: tpd.Tree,
+    params: List[?],
+    actualVarargCount: Int,
+  )(using ctx: Context): Unit = {
+    val atLeastAnnotClass = Symbols.getClassIfDefined("com.avsystem.commons.annotation.atLeast")
+
+    if (atLeastAnnotClass != NoSymbol) {
+      val methodParams = fun.symbol.paramSymss.flatten
+      val lastParamSym = methodParams.lastOption
+
+      lastParamSym.foreach { paramSym =>
+        paramSym.annotations.foreach { annot =>
+          if (annot.symbol == atLeastAnnotClass) {
+            val required = annot.arguments match {
+              case List(tpd.Literal(Constant(n: Int))) => n
+              case _ => 0
+            }
+
+            if (actualVarargCount < required) {
+              report(
+                tree,
+                s"This method requires at least $required arguments for its repeated parameter, $actualVarargCount passed.",
+              )
+            }
+          }
+        }
+      }
+    }
   }
 }
---
+++ analyzer/src/test/scala/com/avsystem/commons/analyzer/VarargsAtLeastTest.scala
@@ -0,0 +1,50 @@
+package com.avsystem.commons
+package analyzer
+
+import org.scalatest.funsuite.AnyFunSuite
+
+final class VarargsAtLeastTest extends AnyFunSuite with AnalyzerTest {
+
+  // Test-local @atLeast annotation since commons-core is not on analyzer test classpath
+  private val atLeastAnnotDef = """
+    |package com.avsystem.commons.annotation
+    |import scala.annotation.StaticAnnotation
+    |class atLeast(n: Int) extends StaticAnnotation
+    |""".stripMargin
+
+  private val testUtilsDef = """
+    |import com.avsystem.commons.annotation.atLeast
+    |object TestUtils {
+    |  def need3Params(@atLeast(3) args: Any*): Unit = ()
+    |}
+    |""".stripMargin
+
+  test("too few varargs parameters should be rejected") {
+    assertErrors(
+      1,
+      atLeastAnnotDef + scala"""
+        |$testUtilsDef
+        |TestUtils.need3Params(1, 2)
+        |""".stripMargin,
+    )
+  }
+
+  test("enough varargs parameters should not be rejected") {
+    assertNoErrors(
+      atLeastAnnotDef + scala"""
+        |$testUtilsDef
+        |TestUtils.need3Params(1, 2, 3)
+        |TestUtils.need3Params(1, 2, 3, 4)
+        |""".stripMargin,
+    )
+  }
+
+  test("collection passed as varargs parameter should not be rejected") {
+    assertNoErrors(
+      atLeastAnnotDef + scala"""
+        |$testUtilsDef
+        |TestUtils.need3Params(List(1, 2)*)
+        |""".stripMargin,
+    )
+  }
+}
