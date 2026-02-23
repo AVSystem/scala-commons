bd3dae115bbe542b346c95a571a28b826d631bb8
Bartłomiej Kozak <b.kozak@avsystem.com>
2026-02-23 18:15:52 +0100

    deprecate org.mongodb.scala dependencies in preparation for removal

--- mongo/jvm/src/main/scala/com/avsystem/commons/mongo/scala/GenCodecCollection.scala
+++ mongo/jvm/src/main/scala/com/avsystem/commons/mongo/scala/GenCodecCollection.scala
@@ -5,6 +5,7 @@ import com.avsystem.commons.mongo.core.GenCodecRegistry
 import com.avsystem.commons.serialization.GenCodec
 import org.mongodb.scala.{MongoCollection, MongoDatabase}
 
+@deprecated("Dependency on org.mongodb.scala will be removed", "2.27.0")
 object GenCodecCollection {
 
   def create[T: ClassTag: GenCodec](
