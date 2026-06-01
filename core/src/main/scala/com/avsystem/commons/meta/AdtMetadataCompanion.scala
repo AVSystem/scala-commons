3e50170956a1d3bebb1d77ea6f62213f7ce27b01
Bartłomiej Kozak <b.kozak@avsystem.com>
2026-06-02 00:53:16 +0200

    feat(scala-3,core): port AdtMetadataCompanion + BoundedAdtMetadataCompanion
    Translated from origin/master:core/src/main/scala-3/com/avsystem/commons/meta/AdtMetadataCompanion.scala.
    
    Collapses our Phase-1 4-method stub into fork's 2-line trait composition:
    - trait AdtMetadataCompanion[M[X] <: TypedMetadata[X]] extends
        AdtMetadataCompanionMacros[M] with MetadataCompanion[M]
    - trait BoundedAdtMetadataCompanion[Hi, Lo <: Hi, M[_ >: Lo <: Hi]] extends
        BoundedAdtMetadataCompanionMacros[Hi, Lo, M] with BoundedMetadataCompanion[Hi, Lo, M]
    
    API shape shift: bound tightened from M[_] to M[X] <: TypedMetadata[X]. Documented
    in MIGRATION.md §3 (source-compat) + §4 (bincompat) — separate commit.
    
    Rationale (fork): enables inline given [T] => M[T] = materialize[T] in
    AdtMetadataCompanionMacros — the bound is required for the inherited inline given
    to resolve correctly.
    
    Fork "must copy" comment on BoundedAdtMetadataCompanion preserved verbatim.

--- core/src/main/scala/com/avsystem/commons/meta/AdtMetadataCompanion.scala
+++ core/src/main/scala/com/avsystem/commons/meta/AdtMetadataCompanion.scala
@@ -11,13 +11,7 @@ package meta
   * @tparam M
   *   metadata class constructor
   */
-trait AdtMetadataCompanion[M[_]] extends MetadataCompanion[M] {
-  // TODO[scala3-port]: materialize (Scala 2 macro def) (L)
-  def materialize[T]: M[T] = ???
-
-  // TODO[scala3-port]: fromApplyUnapplyProvider (Scala 2 macro def) (L)
-  def fromApplyUnapplyProvider[T](applyUnapplyProvider: Any): M[T] = ???
-}
+trait AdtMetadataCompanion[M[X] <: TypedMetadata[X]] extends AdtMetadataCompanionMacros[M] with MetadataCompanion[M] {}
 
 /** Like [[AdtMetadataCompanion]] but allows the metadata class' type parameter to be bounded.
   *
@@ -29,10 +23,5 @@ trait AdtMetadataCompanion[M[_]] extends MetadataCompanion[M] {
   *   metadata class type constructor
   */
 // cannot share code with AdtMetadataCompanion because of binary compatibility problems, must copy
-trait BoundedAdtMetadataCompanion[Hi, Lo <: Hi, M[_ >: Lo <: Hi]] extends BoundedMetadataCompanion[Hi, Lo, M] {
-  // TODO[scala3-port]: materialize (bounded) (Scala 2 macro def) (L)
-  def materialize[T >: Lo <: Hi]: M[T] = ???
-
-  // TODO[scala3-port]: fromApplyUnapplyProvider (bounded) (Scala 2 macro def) (L)
-  def fromApplyUnapplyProvider[T >: Lo <: Hi](applyUnapplyProvider: Any): M[T] = ???
-}
+trait BoundedAdtMetadataCompanion[Hi, Lo <: Hi, M[_ >: Lo <: Hi]]
+  extends BoundedAdtMetadataCompanionMacros[Hi, Lo, M] with BoundedMetadataCompanion[Hi, Lo, M] {}
