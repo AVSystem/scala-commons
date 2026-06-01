9c653bcbb1a2c02260a73f64514c546c1b6b541e
Bartłomiej Kozak <b.kozak@avsystem.com>
2026-06-01 18:07:52 +0200

    refactor(scala-3,core): extract ImplicitNotFound to its own file
    Separates the still-useful ImplicitNotFound sealed trait + companion from
    the to-be-deleted Implicits object. Pure extraction — semantics unchanged.

--- core/src/main/scala/com/avsystem/commons/misc/Implicits.scala
+++ core/src/main/scala/com/avsystem/commons/misc/Implicits.scala
@@ -9,37 +9,3 @@ object Implicits {
   // TODO[scala3-port]: inferNonMacro (Scala 2 macro def) (L)
   def inferNonMacro[T](clue: String): T = ???
 }
-
-/** Extends the functionality of [[scala.annotation.implicitNotFound]] so that implicit-not-found error message is
-  * itself resolved using implicit search. This mechanism is used by [[Implicits.infer[T]:T*]] and macro engines that
-  * use it.
-  *
-  * Example: we have a wrapper class `Box[T]` and we want a custom error message when `GenCodec[Box[T]]` for some type
-  * `T` is not found:
-  *
-  * {{{
-  *   trait Box[T]
-  *   object Box {
-  *     implicit def boxCodec[T: GenCodec]: GenCodec[Box[T]] = ...
-  *
-  *     @implicitNotFound("GenCodec for Box[$${T}] not found. This is most likely caused by lack of GenCodec[$${T}]")
-  *     implicit def boxCodecNotFound[T]: ImplicitNotFound[GenCodec[Box[T]]] = ImplicitNotFound()
-  *   }
-  * }}}
-  *
-  * It is also possible to compose error message for one type from error messages for other types. The example above
-  * could reuse the implicit-not-found message for `GenCodec[T]` when building the message for `GenCodec[Box[T]]`:
-  *
-  * {{{
-  *   @implicitNotFound("GenCodec for Box[$${T}] not found, because:\n#{forUnboxed}")
-  *   implicit def boxCodecNotFound[T](
-  *     implicit forUnboxed: ImplicitNotFound[GenCodec[T]]
-  *   ): ImplicitNotFound[GenCodec[Box[T]]] = ImplicitNotFound()
-  * }}}
-  */
-sealed trait ImplicitNotFound[T]
-object ImplicitNotFound {
-  def apply[T](): ImplicitNotFound[T] = throw new NotImplementedError("ImplicitNotFound.apply")
-
-  implicit def dummy[T]: ImplicitNotFound[T] = apply()
-}
