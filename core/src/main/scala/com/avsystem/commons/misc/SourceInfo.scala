aa7d3d29623a6e7ff70e47c6cc25579d6c5169b7
Bartłomiej Kozak <b.kozak@avsystem.com>
2026-06-02 11:40:09 +0200

    feat(scala-3,core): port SourceInfo.here macro
    Wire SourceInfo.here to inline implicit def backed by Position.ofMacroExpansion + Symbol.spliceOwner.owner enclosing walk. Replaces Phase-1 ??? stub. Public signature unchanged.

--- core/src/main/scala/com/avsystem/commons/misc/SourceInfo.scala
+++ core/src/main/scala/com/avsystem/commons/misc/SourceInfo.scala
@@ -1,6 +1,9 @@
 package com.avsystem.commons
 package misc
 
+import scala.annotation.tailrec
+import scala.quoted.*
+
 /** Macro-materialized implicit value that provides information about callsite source file position. It can be used in
   * runtime for logging and debugging purposes. Similar to Scalactic's `Position`, but contains more information.
   */
@@ -25,6 +28,31 @@ case class SourceInfo(
 object SourceInfo {
   def apply()(implicit si: SourceInfo): SourceInfo = si
 
-  // TODO[scala3-port]: SourceInfo.here (Scala 2 macro def) (L)
-  implicit def here: SourceInfo = ???
+  inline implicit def here: SourceInfo = ${ hereImpl }
+
+  private[misc] def hereImpl(using quotes: Quotes): Expr[SourceInfo] = {
+    import quotes.reflect.*
+    val pos = Position.ofMacroExpansion
+
+    @tailrec
+    def enclosingLoop(sym: Symbol, acc: Vector[String]): Seq[String] =
+      if (sym == defn.RootClass) acc
+      else enclosingLoop(sym.owner, acc :+ sym.name)
+
+    '{
+      SourceInfo(
+        ${ Expr(pos.sourceFile.path) },
+        ${ Expr(pos.sourceFile.name) },
+        ${ Expr(pos.start) },
+        ${ Expr(pos.startLine + 1) },
+        ${ Expr(pos.startColumn + 1) },
+        ${
+          Expr(
+            pos.sourceFile.content.flatMap(_.linesIterator.drop(pos.startLine).nextOption).getOrElse("<no content>")
+          )
+        },
+        ${ Expr(enclosingLoop(Symbol.spliceOwner.owner, Vector.empty).toList) },
+      )
+    }
+  }
 }
