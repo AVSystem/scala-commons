dbab878811a9dd667c61b30a535643baa6fc35cd
Bartłomiej Kozak <b.kozak@avsystem.com>
2026-06-02 14:18:34 +0200

    refactor(scala-3,core): delete Bidirectional macro stub + test

--- core/src/main/scala/com/avsystem/commons/misc/Bidirectional.scala
+++
@@ -1,8 +0,0 @@
-package com.avsystem.commons
-package misc
-
-/** Creates reversed partial function. */
-object Bidirectional {
-  // TODO[scala3-port]: apply (Scala 2 macro def) (L)
-  def apply[A, B](pf: PartialFunction[A, B]): (PartialFunction[A, B], PartialFunction[B, A]) = ???
-}
