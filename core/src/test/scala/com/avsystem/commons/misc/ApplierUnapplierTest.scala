f086b7dd0e29eddba6493af2b4c8faca70976ed7
Bartłomiej Kozak <bartlomiejkozak@proton.me>
2026-06-15 18:50:46 +0200

    Refactor `ApplierUnapplierTest` to use updated Scala 3 syntax and rename `WithOpt` to `WithOption`.

--- core/src/test/scala/com/avsystem/commons/misc/ApplierUnapplierTest.scala
+++ core/src/test/scala/com/avsystem/commons/misc/ApplierUnapplierTest.scala
@@ -5,60 +5,55 @@ import org.scalactic.source.Position
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-case class Inner(s: String)
-case class Outer(inner: Inner, n: Int)
-case class WithOpt(o: Option[String], i: Int)
-
-sealed trait Animal
-case object Dog extends Animal
-case object Cat extends Animal
-
-case class Empty()
-case class Single(str: String)
-case class Multiple(str: String, int: Int)
-case class Varargs(str: String, ints: Int*)
-case class Generic[T](str: String, value: T)
-
-case class Over22(
-  p01: String = "01",
-  p02: String = "02",
-  p03: String = "03",
-  p04: String = "04",
-  p05: String = "05",
-  p06: String = "06",
-  p07: String = "07",
-  p08: String = "08",
-  p09: String = "09",
-  p10: String = "10",
-  p11: String = "11",
-  p12: String = "12",
-  p13: String = "13",
-  p14: String = "14",
-  p15: String = "15",
-  p16: String = "16",
-  p17: String = "17",
-  p18: String = "18",
-  p19: String = "19",
-  p20: String = "20",
-  p21: String = "21",
-  p22: String = "22",
-  p23: String = "23",
-  p24: String = "24",
-  p25: String = "25",
-)
-
-given CustomProductApplierUnapplier: ProductApplierUnapplier[Multiple] = new {
-  override def apply(rawValues: Seq[Any]): Multiple =
-    Multiple(rawValues.head.asInstanceOf[String], rawValues(1).asInstanceOf[Int])
-}
-
-class ApplierUnapplierTest extends AnyFunSuite with Matchers {
+final class ApplierUnapplierTest extends AnyFunSuite with Matchers {
   def roundtrip[T: {Applier as applier, Unapplier as unapplier, ApplierUnapplier as applierUnapplier}](
     value: T
   )(using Position
   ): Unit = {
     assert(value == applier(unapplier.unapply(value)))
     assert(value == applierUnapplier(applierUnapplier.unapply(value)))
+  }
+  sealed trait Animal
+  final case class Inner(s: String)
+  final case class Outer(inner: Inner, n: Int)
+  final case class WithOption(o: Option[String], i: Int)
+  case class Empty()
+  case class Single(str: String)
+  case class Multiple(str: String, int: Int)
+  case class Varargs(str: String, ints: Int*)
+  case class Generic[T](str: String, value: T)
+  case class Over22(
+    p01: String = "01",
+    p02: String = "02",
+    p03: String = "03",
+    p04: String = "04",
+    p05: String = "05",
+    p06: String = "06",
+    p07: String = "07",
+    p08: String = "08",
+    p09: String = "09",
+    p10: String = "10",
+    p11: String = "11",
+    p12: String = "12",
+    p13: String = "13",
+    p14: String = "14",
+    p15: String = "15",
+    p16: String = "16",
+    p17: String = "17",
+    p18: String = "18",
+    p19: String = "19",
+    p20: String = "20",
+    p21: String = "21",
+    p22: String = "22",
+    p23: String = "23",
+    p24: String = "24",
+    p25: String = "25",
+  )
+  case object Dog extends Animal
+  case object Cat extends Animal
+  given CustomProductApplierUnapplier: ProductApplierUnapplier[Multiple] {
+    override def apply(rawValues: Seq[Any]): Multiple =
+      Multiple(rawValues.head.asInstanceOf[String], rawValues(1).asInstanceOf[Int])
   }
 
   test("no params") {
@@ -86,8 +81,8 @@ class ApplierUnapplierTest extends AnyFunSuite with Matchers {
     roundtrip(Outer(Inner("inside"), 7))
   }
   test("case class with Option field") {
-    roundtrip(WithOpt(Some("x"), 1))
-    roundtrip(WithOpt(None, 2))
+    roundtrip(WithOption(Some("x"), 1))
+    roundtrip(WithOption(None, 2))
   }
   test("sealed trait does not derive") {
     "summon[Applier[Animal]]" shouldNot compile
